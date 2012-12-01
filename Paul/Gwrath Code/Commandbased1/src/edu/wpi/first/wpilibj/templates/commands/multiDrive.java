dbe8041f8c5fa13d5eca6f84e2d0cfefc56d527b
Student <Student@TP-2012-06>
2012-12-01 14:28:49 -0800

    Code for Gwrath. Controls the drivetrain. Can move autonomously using gyro/encoders. Arm is not implemented yet.

---
+++ Paul/Gwrath Code/Commandbased1/src/edu/wpi/first/wpilibj/templates/commands/multiDrive.java
@@ -0,0 +1,47 @@
+//Switches between drives using sendable chooser
+package edu.wpi.first.wpilibj.templates.commands;
+
+import edu.wpi.first.wpilibj.smartdashboard.*;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.templates.OI;
+
+public class multiDrive extends CommandBase {
+
+    public SendableChooser driveChooser;
+
+    public multiDrive() {
+        requires(drivetrain);
+        driveChooser = new SendableChooser();
+        driveChooser.addDefault("Tank Drive", "tank");
+        driveChooser.addObject("One Joystick Arcade Drive", "arcade1");
+        driveChooser.addObject("Two Joystick Arcade Drive", "arcade2");
+        driveChooser.addObject("Gyro Drive Forward", "gyro");
+        SmartDashboard.putData("Drive Mode", driveChooser);
+    }
+
+    protected void initialize() {
+    }
+
+    protected void execute() {
+        if (driveChooser.getSelected().equals("tank")){
+            drivetrain.tankdrive(OI.getLeftJoystick(), OI.getRightJoystick());
+        }
+        if (driveChooser.getSelected().equals("arcade1")){
+            drivetrain.arcadedrive1(OI.getLeftJoystick());
+        }
+        if (driveChooser.getSelected().equals("arcade2")){
+            drivetrain.arcadedrive2(OI.getLeftJoystick(), OI.getRightJoystick());
+        }
+        drivetrain.putdata();
+    }
+
+    protected boolean isFinished() {
+        return false;
+    }
+
+    protected void end() {
+    }
+
+    protected void interrupted() {
+    }
+}
