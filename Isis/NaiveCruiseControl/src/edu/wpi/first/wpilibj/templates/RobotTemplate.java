0f574fbe9f7e4867027fbb689de0904f5cfd2772
Isis Grant <img798@gmail.com>
2012-12-31 11:13:52 -0800

    Adding new version of cruise control and a feedforward calibration program. Also fixed bugs and added some functions to Naive and v2.

--- Isis/NaiveCruiseControl/src/edu/wpi/first/wpilibj/templates/RobotTemplate.java
+++ Isis/NaiveCruiseControl/src/edu/wpi/first/wpilibj/templates/RobotTemplate.java
@@ -20,122 +20,121 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
  * directory.
  */
 public class RobotTemplate extends IterativeRobot {
-
-    /**
-     * This function is run when the robot is first started up and should be
-     * used for any initialization code.
-     */
+    //Robot objects
     private final Jaguar jaguarLeft = new Jaguar(3);
     private final Jaguar jaguarRight = new Jaguar(2);
     private final Encoder encoderLeft = new Encoder(7, 6);
     private final Encoder encoderRight = new Encoder(2, 1);
     private final Timer timer = new Timer();
-    private double currInstVeloc = 0.0; //instantaneous velocity
-    private double velocSetpt = 0.0; //desired or goal velocity
+    //Current value variables
     private double output = 0.0; //PWM signal output to Jaguars
+    //Previous value variables
     private double prevInstVeloc = 0.0;
     private double prevDeriv = 0.0;
-    private double currDeriv = 0.0;
+    private double prevDist = 0.0; //previous encoder count
+    private double prevTime = 0.0; //in s
+    //Quality metrics
     private double numOscills = 0.0; //number of oscillations in velocity
     private double maxVeloc = 0.0; // max recorded inst veloc
-    private double distance = 0.0; //average between raw counts of both encoders
-    private double prevCount = 0.0; //previous encoder count
-    private double currCount = 0.0; //current encoder count
-    private double totalError = 0.0; //sum of epsilons between currInstVeloc and velocSetpt
-    private double loopPeriod = 0.0; //in ms
-    private double prevTime = 0.0; //in ms
-    private double currTime = 0.0;//in ms
-    private double kTolerance = 0.05; //in velocity, ft/s
-    private double kIncrement = 0.05; //in PWM signal to Jaguars
-    private double kGearRatio = 250 * 4 * (27.0 / 13.0) * (0.5 * 3.14159) / 2;
+    private double totalError = 0.0; //sum of epsilons between currInstVeloc and kVelocSetpt
+    private double converge = 0.0; //first time achieve within tolerance
+    //Tuneable constants
+    private double kVelocSetpt = 0.0; //desired or goal velocity
+    private double kTolerance = 0.01; //in velocity, ft/s
+    private double kIncrement = 0.005; //in PWM signal to Jaguars
+    private double kDesPeriod = 0.02; //desired loop period
+    private double kMaxDuration = 10.0;
+    //Constants
+    private final double kMaxOutput = 1.0;
+    private final double kMinOutput = 0.05;
+    private final double kGearRatio = 250 * 4 * (27.0 / 13.0) * (0.5 * 3.14159) / 2;
     //encoder ticks*(quadrature)*gearRatio*circumference*conversion to feet
-    private double kDesPeriod = 1.0; //desired loop period
 
     public void robotInit() {
-        resetInit();
-        SmartDashboard.putDouble("Velocity_Setpoint", 0.0);
-        SmartDashboard.putDouble("kTolerance", 0.05);
-        SmartDashboard.putDouble("kIncrement", 0.05);
-        SmartDashboard.putDouble("kDesPeriod", 1.0);
         encoderLeft.setReverseDirection(true);
         encoderLeft.setDistancePerPulse(1.0);
         encoderRight.setDistancePerPulse(1.0);
+        resetInit();
+        SmartDashboard.putDouble("Velocity_Setpoint", 0.0);
+        SmartDashboard.putDouble("kTolerance", 0.01);
+        SmartDashboard.putDouble("kIncrement", 0.005);
+        SmartDashboard.putDouble("kDesPeriod", 0.02);
+        SmartDashboard.putDouble("kMaxDuration", 10.0);
+    }
+
+    public void autonomousInit() {
+        resetInit();
+    }//end autonomousInit()
+
+    private void resetInit() {
+        output = 0.0;
+        //Robot object resets
+        jaguarLeft.set(0.0);
+        jaguarRight.set(0.0);
         encoderLeft.reset();
         encoderRight.reset();
         encoderLeft.start();
         encoderRight.start();
         timer.reset();
         timer.start();
-    }
-
-    /**
-     * This function is called periodically during autonomous
-     */
-    public void autonomousInit() {
-        resetInit();
-    }//end autonomousInit()
-
-    private void resetInit() {
-        currInstVeloc = 0.0;
-        velocSetpt = 0.0;
-        output = 0.0;
-        distance = 0.0;
-        prevCount = 0.0;
-        currCount = 0.0;
+        //Previous variable resets
+        prevInstVeloc = 0.0;
+        prevDeriv = 0.0;
+        prevDist = 0.0; 
+        prevTime = 0.0; 
+        //Quality metric resets
+        numOscills = 0.0; 
+        maxVeloc = 0.0; 
         totalError = 0.0;
-        loopPeriod = 0.0;
-        prevTime = 0.0;
-        currTime = 0.0;
+        converge = 0.0;
     }// end resetInit()
 
     public void autonomousContinuous() {
         //check if enough time has elapsed
         kDesPeriod = SmartDashboard.getDouble("kDesPeriod", kDesPeriod);
-        currTime = timer.get();
-        loopPeriod = currTime - prevTime;
+        kMaxDuration = SmartDashboard.getDouble("kMaxDuration", kMaxDuration);
+        double currTime = timer.get();
+        double loopPeriod = currTime - prevTime;
         if (loopPeriod < kDesPeriod) {
+            return;
+        } else if (currTime > kMaxDuration){
+            timer.stop();
+            encoderLeft.stop();
+            encoderRight.stop();
+            jaguarLeft.set(0.0);
+            jaguarRight.set(0.0);
             return;
         }
 
         //check for updated values
-        velocSetpt = SmartDashboard.getDouble("Velocity_Setpoint", 0.5);
-        kTolerance = SmartDashboard.getDouble("kTolerance", 0.05);
-        kIncrement = SmartDashboard.getDouble("kIncrement", 0.05);
+        kVelocSetpt = SmartDashboard.getDouble("Velocity_Setpoint", 0.0);
+        kTolerance = SmartDashboard.getDouble("kTolerance", 0.01);
+        kIncrement = SmartDashboard.getDouble("kIncrement", 0.005);
 
         //calculate instantaneous velocity
-        distance = encoderRight.getRaw() / kGearRatio;
-        currCount = distance;
-        double deltaCount = currCount - prevCount;
-        currInstVeloc = deltaCount / loopPeriod;
-        totalError += Math.abs(velocSetpt - currInstVeloc);
-
-        //print to SmartDashboard
-        SmartDashboard.putDouble("Total_error", totalError);
-        SmartDashboard.putDouble("Current_Time", currTime);
-        SmartDashboard.putDouble("encoderLeft_Raw", encoderLeft.getRaw());
-        SmartDashboard.putDouble("encoderRight_Raw", encoderRight.getRaw());
-        SmartDashboard.putDouble("Distance", distance);
-        SmartDashboard.putDouble("Actual_VelocSetpt", velocSetpt);
-        SmartDashboard.putDouble("InstVeloc", currInstVeloc);
-
-        //print to console
-        System.out.println("Total error: " + totalError + ", " + "Loop period: " + loopPeriod);
-        System.out.println(velocSetpt + ", " + kTolerance + ", " + kIncrement);
+        double currDist = encoderRight.getRaw() / kGearRatio;
+        double deltaCount = currDist - prevDist;
+        double currInstVeloc = deltaCount / loopPeriod;
+        totalError += Math.abs((kVelocSetpt - currInstVeloc)*loopPeriod);
 
         //if velocity is within tolerance of velocity, stay same. Else, increase or decrease speed to reach velocity setpoint
-        if (currInstVeloc < kTolerance + velocSetpt && currInstVeloc > velocSetpt - kTolerance) {
+        if (currInstVeloc < kTolerance + kVelocSetpt && currInstVeloc > kVelocSetpt - kTolerance) {
             output = output;
-        } else if (currInstVeloc < velocSetpt) {
+            if (converge == 0.0 && currInstVeloc > kVelocSetpt - kTolerance){
+                converge = timer.get();
+                SmartDashboard.putDouble("Convergence Time", converge);
+            }
+        } else if (currInstVeloc < kVelocSetpt) {
             output += kIncrement;
-        } else if (currInstVeloc > velocSetpt) {
+        } else if (currInstVeloc > kVelocSetpt) {
             output -= kIncrement;
         }
 
         //keep in single direction of motion, eliminate deadband
-        if (output > 1.0) {
-            output = 1.0;
-        } else if (output < 0.05) {
-            output = 0.05;
+        if (output > kMaxOutput) {
+            output = kMaxOutput;
+        } else if (output < kMinOutput) {
+            output = kMinOutput;
         }
 
         //Move!
@@ -144,21 +143,34 @@ public class RobotTemplate extends IterativeRobot {
         SmartDashboard.putDouble("Output", output);
 
         //Gather data for number of oscillations in output and max currInstVeloc
-        currDeriv = currInstVeloc - prevInstVeloc;
+        double currDeriv = currInstVeloc - prevInstVeloc;
         if ((prevDeriv > 0.0 && currDeriv < 0.0) || (prevDeriv < 0.0 && currDeriv > 0.0)) {
             numOscills++;
         }
-        if (currInstVeloc > prevInstVeloc && currInstVeloc > maxVeloc) {
+        if (currInstVeloc > maxVeloc) {
             maxVeloc = currInstVeloc;
         }
+                
+        //print to SmartDashboard
+        SmartDashboard.putDouble("Total_error", totalError);
+        SmartDashboard.putDouble("Current_Time", currTime);
+        SmartDashboard.putDouble("encoderLeft_Raw", encoderLeft.getRaw());
+        SmartDashboard.putDouble("encoderRight_Raw", encoderRight.getRaw());
+        SmartDashboard.putDouble("Distance", currDist);
+        SmartDashboard.putDouble("Actual_VelocSetpt", kVelocSetpt);
+        SmartDashboard.putDouble("InstVeloc", currInstVeloc);
+        SmartDashboard.putDouble("Max Error", maxVeloc - kVelocSetpt);
         SmartDashboard.putDouble("numOscills", numOscills);
-        SmartDashboard.putDouble("Diff_btwn_actual_&_goal_veloc", maxVeloc - velocSetpt);
 
+        //print to console
+        System.out.println("Total error: " + totalError + ", " + "Loop period: " + loopPeriod);
+        System.out.println(kVelocSetpt + ", " + kTolerance + ", " + kIncrement);
+        
         //setup for next loop
         prevDeriv = currDeriv;
         prevInstVeloc = currInstVeloc;
         prevTime = currTime;
-        prevCount = currCount;
+        prevDist = currDist;
         
     }//end autonomousPeriodic()
 }//end RobotTemplate
