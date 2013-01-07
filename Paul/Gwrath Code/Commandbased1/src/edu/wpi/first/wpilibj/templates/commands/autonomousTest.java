b8de5b2f49d35bd6804dd18a8cca841e1d9351ed
student <student@TP-2012-07>
2013-01-06 16:36:12 -0800

    Hammerhead code that was changed to work Kraken's drivetrain

---
+++ Paul/Gwrath Code/Commandbased1/src/edu/wpi/first/wpilibj/templates/commands/autonomousTest.java
@@ -0,0 +1,27 @@
+//Sample autonomous program that turns a corner
+package edu.wpi.first.wpilibj.templates.commands;
+
+public class autonomousTest extends CommandBase {
+    public autonomousTest() {
+        requires(drivetrain);
+    }
+
+    protected void initialize() {
+        drivetrain.resetsensors();
+
+    }
+
+    protected void execute() {
+        drivetrain.autodriveturn(90);
+    }
+
+    protected boolean isFinished() {
+        return false;
+    }
+
+    protected void end() {
+    }
+
+    protected void interrupted() {
+    }
+}
