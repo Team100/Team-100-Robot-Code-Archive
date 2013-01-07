994563141292788bac123f48709aaf6ee45b3333
student <student@TP-2012-07.seq.org>
2013-01-07 11:22:22 -0800

    As the name suggests, an arcade drive version of Kraken's coding

---
+++ Gabe/Kraken_Arcade_Drive/src/edu/wpi/first/wpilibj/templates/subsystems/DriveTrain.java
@@ -0,0 +1,38 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.wpi.first.wpilibj.templates.subsystems;
+
+import edu.wpi.first.wpilibj.Jaguar;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.templates.RobotMap;
+
+/**
+ *
+ * @author student
+ */
+public class DriveTrain extends Subsystem {
+    // Put methods for controlling this subsystem
+    // here. Call these from Commands.
+    Jaguar rightA;
+    Jaguar leftA;
+    RobotDrive drive;
+    
+    public DriveTrain(){
+        rightA = new Jaguar(RobotMap.PWM2_rightDrive);
+        leftA = new Jaguar(RobotMap.PWM3_leftDrive);
+        drive = new RobotDrive(leftA, rightA);
+    }
+    public void arcadeDrive(Joystick left, Joystick right){
+        drive.arcadeDrive(-right.getX(), -left.getY());
+        
+    }
+
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        //setDefaultCommand(new MySpecialCommand());
+    }
+}
