1a1a4430770d4996f5d73a7c366197d59e8c322f
Sam Bunk <sambunk@comcast.net>
2013-01-07 13:59:55 -0800

    Extended the thread pool example to include a callable.

--- Sam/ThreadPoolTest/src/threadpooltest/ThreadPoolTest.java
+++ Sam/ThreadPoolTest/src/threadpooltest/ThreadPoolTest.java
@@ -13,17 +13,39 @@ import edu.emory.mathcs.backport.java.util.concurrent.Executors;
  */
 public class ThreadPoolTest {
     public static void main(String[] args) {
-        int numWorkers = 5;
-        int threadPoolSize = 2;
+        int numWorkers = 2;         // This is the number of things starting in the queue
+        int threadPoolSize = 2;   // Used only if the number of threads is limited
     
+         
         ExecutorService tpes =
-            Executors.newFixedThreadPool(threadPoolSize);
+            
+            //Executors.newCachedThreadPool();                //Thread pool with unlimited threads it will use existing
+                                                            //threads if they are avaliable.
+        
+            //Executors.newCachedThreadPool(ThreadFactory)  //Thread pool with unlimited size, it uses a 
+                                                            //ThreadFactory to create the threads.
+        
+            Executors.newFixedThreadPool(threadPoolSize); //Thread pool with a limited number of threads.
+        
+            //Executors.newFixedThreadPool(threadPoolSize, ThreadFactory) //Limited numbers of threads created
+                                                            //by a thread factory
+        
+
+        
     
         WorkerThread[] workers = new WorkerThread[numWorkers];
         for (int i = 0; i < numWorkers; i++)  {
             workers[i] = new WorkerThread(i);
-            tpes.execute(workers[i]);
+            tpes.execute(workers[i]);//This executes workers(1) in an availiable thread
         }
-        tpes.shutdown();
+        
+        CallableWorker callworker = new CallableWorker();
+        
+        
+        for (int i = 0; i < 5; i++)  {
+            tpes.submit(callworker);
+        }
+        
+        //tpes.shutdown();           //Ends the thread pool
     }
 }
