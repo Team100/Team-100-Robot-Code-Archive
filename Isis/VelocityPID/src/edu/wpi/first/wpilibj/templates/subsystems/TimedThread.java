5c41af6356ee8bdd6019a4071fd9f0c82b761723
Koosha Jahani <Koosha Jahani@localhost>
2013-01-13 11:34:41 -0800

    Merge

--- Isis/VelocityPID/src/edu/wpi/first/wpilibj/templates/subsystems/TimedThread.java
+++ Isis/VelocityPID/src/edu/wpi/first/wpilibj/templates/subsystems/TimedThread.java
@@ -15,20 +15,19 @@ public class TimedThread implements Runnable {
     Callable m_callable;
     int m_period_ms;
 
-    public TimedThread(Callable callable, int period_ms)  {
+    public TimedThread(Callable callable, int period_ms) {
         this.m_callable = callable;
         this.m_period_ms = period_ms;
     }
 
-    public void run(){
-         while (true) {
+    public void run() {
+        while (true) {
             m_callable.call();
             Timer.delay((double) m_period_ms / 1000.0);
         }
     }
-    
-    
+
     public void start() {
-        new Thread(this).start(); 
+        new Thread(this).start();
     }
 }//end TimedThread
