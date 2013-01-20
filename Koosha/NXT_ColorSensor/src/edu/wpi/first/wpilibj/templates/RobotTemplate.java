e887bac23f428bda85c491435c2b5b84ca766445
Student <Student@Student-PC.seq.org>
2013-01-20 09:31:49 -0800

    Merge

--- Koosha/NXT_ColorSensor/src/edu/wpi/first/wpilibj/templates/RobotTemplate.java
+++ Koosha/NXT_ColorSensor/src/edu/wpi/first/wpilibj/templates/RobotTemplate.java
@@ -20,11 +20,12 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
  */
 public class RobotTemplate extends IterativeRobot {
     
-    HiTechnicColorSensor colorSensor;
-    byte colorVal;
-    int red;
-    int blue;
-    int green;
+    HiTechnicColorSensor colorSensor; // used to sense frisbee color; best if used on bottom of frisbee
+    int colorVal;
+    int frisbeeType;
+    String frisbeeOwner;
+    int alliance;
+    Jaguar motor1;
     
     /**
      * This function is run when the robot is first started up and should be
@@ -33,9 +34,9 @@ public class RobotTemplate extends IterativeRobot {
     public void robotInit()
     {
         colorSensor = new HiTechnicColorSensor(1);
-        red=0;
-        blue=0;
-        green=0;
+        frisbeeType=3; //0->red, 1->blue, 2->white, 3->default
+        frisbeeOwner="Unknown";
+        motor1 = new Jaguar(2);
     }
 
     /**
@@ -44,21 +45,59 @@ public class RobotTemplate extends IterativeRobot {
     public void autonomousPeriodic() {
 
     }
-
+    
     /**
      * This function is called periodically during operator control
      */
     public void teleopPeriodic()
     {
+        alliance=DriverStation.getInstance().getAlliance().value;
         colorVal = colorSensor.getColor();
-        red = colorSensor.getRed();
-        blue = colorSensor.getBlue();
-        green = colorSensor.getGreen();
-        SmartDashboard.putString("ColorSensor", colorSensor.getSmartDashboardType());
+        
+        if(((colorVal+1)%11)>=9) //will return true for 8, and 9
+        {
+            frisbeeType=0;
+        }
+        else if((Math.abs((colorVal-2)%8.5))<=1) //will return true for 1, 2, 3, and 11
+        {
+            frisbeeType=1;
+        }
+        else if(colorVal==17)
+        {
+            frisbeeType=2;
+        }
+        else if(colorVal==0)
+        {
+            frisbeeType=3;
+        }
+        else
+        {
+            frisbeeType=3;
+        }
+        
+        if(frisbeeType==alliance)
+        {
+            frisbeeOwner="Ours";
+        }
+        else if(frisbeeType==(alliance^1))
+        {
+            frisbeeOwner="Theirs";
+        }
+        else if(frisbeeType==2)
+        {
+            frisbeeOwner="Neutral";
+        }
+        else
+        {
+            frisbeeOwner="Unknown";
+        }
+        
+        motor1.set(0.5);
         SmartDashboard.putNumber("Color Sensor Value", colorVal);
-        SmartDashboard.putNumber("Red", red);
-        SmartDashboard.putNumber("Blue", blue);
-        SmartDashboard.putNumber("Green", green);
+        SmartDashboard.putString("Alliance Color", DriverStation.getInstance().getAlliance().name);
+        SmartDashboard.putNumber("Alliance Value", alliance);
+        SmartDashboard.putNumber("Frisbee Number", frisbeeType);
+        SmartDashboard.putString("Frisbee Owner", frisbeeOwner);
     }
     
     /**
