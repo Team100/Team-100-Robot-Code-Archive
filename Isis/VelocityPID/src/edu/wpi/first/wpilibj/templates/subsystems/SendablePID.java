159beec1a889efec80b4385504b3896a1476b4b2
Student <Student@Student-PC.seq.org>
2013-01-21 15:54:58 -0800

    Merge

---
+++ Isis/VelocityPID/src/edu/wpi/first/wpilibj/templates/subsystems/SendablePID.java
@@ -0,0 +1,67 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.wpi.first.wpilibj.templates.subsystems;
+
+import edu.wpi.first.wpilibj.PIDOutput;
+import edu.wpi.first.wpilibj.PIDSource;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.Timer;
+
+/**
+ *
+ * @author Isis
+ */
+public class SendablePID {
+    
+    private final PIDSource m_source;
+    private final PIDOutput m_output;
+    private final TimedThread m_thread;
+    private final PIDBase m_base;
+
+    public SendablePID(PIDSource source, PIDOutput output, double distRatio) {
+        m_base = new PIDBase(distRatio);
+        PIDInit();
+        m_source = source;
+        m_output = output;
+        Callable callable = new Callable() {
+            Timer timer = new Timer();
+            public void call() {
+                if (timer.get() == 0.0){
+                    timer.start();
+                    return;
+                }
+                double input = m_source.pidGet();
+                getValues();
+                double result = m_base.calculate(timer.get());
+                timer.reset();
+                m_output.pidWrite(result);
+            }
+        };
+        m_thread = new TimedThread(callable);
+        m_thread.run();
+    }//end SendablePID
+
+    private void PIDInit() {
+        SmartDashboard.putNumber("kP", 0.0);
+        SmartDashboard.putNumber("kI", 0.0);
+        SmartDashboard.putNumber("kD", 0.0);
+        SmartDashboard.putNumber("kMaxOutput", 0.0);
+        SmartDashboard.putNumber("kMinOutput", 0.0);
+    }//end PIDInit
+
+    public void getValues() {
+        m_base.setKP(SmartDashboard.getNumber("kP", 0.0));
+        m_base.setKI(SmartDashboard.getNumber("kI", 0.0));
+        m_base.setKD(SmartDashboard.getNumber("kD", 0.0));
+        m_base.setMaxOutput(SmartDashboard.getNumber("kMaxOutput", 0.0));
+        m_base.setMinOutput(SmartDashboard.getNumber("kminOutput", 0.0));
+    }//end getValues
+    
+    public void setSetpoint(double setpoint) {
+        m_base.setSetpoint(setpoint);
+    }//end setSetpoint
+      
+}//end SendablePID
+
---
+++ Isis/VelocityPID/src/edu/wpi/first/wpilibj/templates/subsystems/TimedThread.java
@@ -0,0 +1,40 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.wpi.first.wpilibj.templates.subsystems;
+
+import edu.wpi.first.wpilibj.Timer;
+
+/**
+ *
+ * @author Isis
+ */
+public class TimedThread implements Runnable {
+
+    Callable m_callable;
+    int m_period_ms = 10;
+
+    public TimedThread(Callable callable) {
+        this.m_callable = callable;
+    }
+
+    public void run() {
+        while (true) {
+            m_callable.call();
+            int period;
+            synchronized (this) {
+                period = m_period_ms;
+            }
+            Timer.delay((double) period / 1000.0);
+        }
+    }
+
+    public void start() {
+        new Thread(this).start();
+    }
+    
+    public synchronized void setPeriod(int period){
+        m_period_ms = period;
+    }
+}//end TimedThread
