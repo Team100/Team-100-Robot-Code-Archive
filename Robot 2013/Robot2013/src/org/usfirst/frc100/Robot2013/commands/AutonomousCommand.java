a32e6ea2dc374dc74f9f9f407fa0af5cf7abc56a
Student <Student@Student-PC.seq.org>
2013-02-02 11:53:50 -0800

    one more error resolved

--- Robot 2013/Robot2013/src/org/usfirst/frc100/Robot2013/commands/AutonomousCommand.java
+++ Robot 2013/Robot2013/src/org/usfirst/frc100/Robot2013/commands/AutonomousCommand.java
@@ -28,7 +28,7 @@ public class  AutonomousCommand extends Command {
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
         Robot.climber.homingSequence();
-        Robot.climber.tiltElevatorForward();
+        Robot.tower.tiltElevatorForward();
     }
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
