064c42c1fc15a08840b61ca8fb4d0fcb72b205bd
Koosha Jahani <Koosha Jahani@localhost>
2013-02-02 15:01:44 -0800

    Configured SendableChooser to allow the driver to pick an autonmous mode while the robot is disabled and even set a delay before the program runs. Also I proved SmartDashboard values can be read even on disabled.

---
+++ Koosha/FrisbeeHandling/src/edu/wpi/first/wpilibj/templates/subsystems/FrisbeeHandler.java
@@ -0,0 +1,19 @@
+
+package edu.wpi.first.wpilibj.templates.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ *
+ */
+public class FrisbeeHandler extends Subsystem {
+    // Put methods for controlling this subsystem
+    // here. Call these from Commands.
+    
+
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        //setDefaultCommand(new MySpecialCommand());
+    }
+}
+
