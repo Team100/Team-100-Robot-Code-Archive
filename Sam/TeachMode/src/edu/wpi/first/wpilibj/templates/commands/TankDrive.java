f1f26bddb5f61077496439184913516844ceaa0c
Student <Student@TP-2012-06.seq.org>
2013-02-09 15:42:32 -0800

    Added a widget to display robot tilt

---
+++ Sam/TeachMode/src/edu/wpi/first/wpilibj/templates/commands/TankDrive.java
@@ -0,0 +1,38 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.wpi.first.wpilibj.templates.commands;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+/**
+ *
+ * @author Sam
+ */
+public class TankDrive extends CommandBase{
+    
+    public TankDrive(){
+        requires(driveTrain);
+    }
+
+    protected void initialize() {
+    }
+
+    protected void execute() {
+        driveTrain.drive(oi.getJoy1_y1(), oi.getJoy1_y2());
+        SmartDashboard.putNumber("Joy1", oi.getJoy1_y1());
+        SmartDashboard.putNumber("Joy2", oi.getJoy1_y2());
+    }
+
+    protected boolean isFinished() {
+        return false;
+    }
+
+    protected void end() {
+    }
+
+    protected void interrupted() {
+    }
+    
+}
