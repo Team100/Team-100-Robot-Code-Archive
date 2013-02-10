a39187660407513623bf54373c730a7d9e4444f8
Student <Student@TP-2012-06.seq.org>
2013-02-09 17:27:21 -0800

    Slight edit

--- Paul/DiscHandling/src/edu/wpi/first/wpilibj/templates/commands/TakeFrisbees.java
+++ Paul/DiscHandling/src/edu/wpi/first/wpilibj/templates/commands/TakeFrisbees.java
@@ -19,7 +19,7 @@ public class TakeFrisbees extends CommandBase {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-        
+        intake.takeFrisbees();
     }
 
     // Make this return true when this Command no longer needs to run execute()
