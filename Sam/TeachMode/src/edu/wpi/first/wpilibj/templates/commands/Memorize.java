249cc6b6d294cc1d270d0442c5d5c7b66962bd3f
Koosha Jahani <Koosha Jahani@localhost>
2013-02-09 18:42:35 -0800

    Merge

--- Sam/TeachMode/src/edu/wpi/first/wpilibj/templates/commands/Memorize.java
+++ Sam/TeachMode/src/edu/wpi/first/wpilibj/templates/commands/Memorize.java
@@ -5,6 +5,7 @@
 package edu.wpi.first.wpilibj.templates.commands;
 
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import java.io.IOException;
 
 /**
  *
@@ -17,12 +18,14 @@ public class Memorize extends CommandBase{
     }
 
     protected void initialize() {
+        System.out.println("Beginning Collection");
         autoMemory.beginCollection();
         SmartDashboard.putBoolean("Collecting", true);
     }
 
     protected void execute() {
-        autoMemory.collect(oi.getJoy1_y1(), oi.getJoy1_y2());
+        //autoMemory.collect(oi.getJoy1_y1(), oi.getJoy1_y2());
+        autoMemory.collectString(oi.getJoy1_y1(), oi.getJoy1_y2());
     }
 
     protected boolean isFinished() {
@@ -30,7 +33,11 @@ public class Memorize extends CommandBase{
     }
 
     protected void end() {
-        autoMemory.stopCollection();
+        try {
+            autoMemory.stopCollection();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
         SmartDashboard.putBoolean("Collecting", false);
     }
 
--- Sam/TeachMode/src/edu/wpi/first/wpilibj/templates/subsystems/AutoMemory.java
+++ Sam/TeachMode/src/edu/wpi/first/wpilibj/templates/subsystems/AutoMemory.java
@@ -7,11 +7,11 @@ package edu.wpi.first.wpilibj.templates.subsystems;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import java.util.Vector;
-import com.sun.squawk.microedition.io.FileConnection;
+import edu.wpi.first.wpilibj.templates.FileRW;
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
-import java.io.OutputStream;
-import javax.microedition.io.Connector;
+import java.util.Vector;
 
 
 /**
@@ -23,21 +23,18 @@ public class AutoMemory extends Subsystem{
     Vector LeftMemory;
     Vector RightMemory;
     
-    
     public AutoMemory(){
     }
 
     public void beginCollection(){
-        LeftMemory = new Vector();
-        RightMemory = new Vector();
+        System.out.println("AutoMemory,beginCollection");
+        LeftMemory  = new Vector();
+        RightMemory  = new Vector();
     }
     
-    public void collect(double left, double right){
-        LeftMemory.addElement(Double.valueOf(left));
-        RightMemory.addElement(Double.valueOf(right));
-    }
-    
-    public void stopCollection(){
+    public void collectString(double left, double right){
+       LeftMemory.addElement(String.valueOf(left));
+       RightMemory.addElement(String.valueOf(right));
     }
     
     public Vector RequestLeft(){
@@ -46,10 +43,6 @@ public class AutoMemory extends Subsystem{
     
     public Vector RequestRight(){
         return RightMemory;
-    }
-    
-    public void ReadMemory(int index){
-        //read from a file and set the vectors equal to the info
     }
     
     public void Reproduce(double leftTarget, double rightTarget){
@@ -62,35 +55,60 @@ public class AutoMemory extends Subsystem{
     protected void initDefaultCommand() {
     }
     
-    private void write(String FILE_NAME){
-        FileConnection file = null;
-        try {
-                file = (FileConnection) Connector.open(FILE_NAME, Connector.WRITE);
-
-                file.create();
-
-                OutputStream output = file.openOutputStream();
-                
-                //output.write(bytes);
-        } catch (IOException ex) {
-                ex.printStackTrace();
-            } finally {
-                if (file != null) {
-                    try {
-                        file.close();
-                    } catch (IOException ex) {
-                    }
-                }
-        }
-    
+    public void stopCollection() throws IOException{
+        this.write("file:///test.sam");
     }
     
+    //FILE_NAME = file:///FILENAME.txt
+    private void write(String FILE_NAME) throws IOException{
+        System.out.println("Beginning writing");
+        byte[] bytes = null;
+        byte[] bytes1 = null;
+        
+        if(LeftMemory.size() != RightMemory.size()){System.out.println("VECTORS NOT EQUAL SIZE!!!!");}
+        //
+        
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        DataOutputStream dos = new DataOutputStream(baos);
+        
+        
+        for(int i=0;i<LeftMemory.size();i++){
+            String Left = (String) LeftMemory.elementAt(i);
+            String Right = (String) RightMemory.elementAt(i);
+            
+            String point = Left + "," + Right;
+            dos.writeUTF(point);
+        }
+        //baos.toByteArray();
+        //FileRW.getInstance().save(FILE_NAME, bytes);
+        System.out.println(baos);
+        FileRW.getInstance().save(FILE_NAME, baos.toByteArray());
+    }
     
-    
-    
-    
-    
-    
-    
-    
+    public void read(String path){
+        System.out.println("Beginning reading");
+        //read from a file and set the vectors equal to the info
+        String data;
+        data = FileRW.newRead(path);//Read from path
+        
+        //Clear Vectors
+        LeftMemory = null;
+        RightMemory = null;
+        
+        boolean complete = false;
+        int beginIndex=0;
+        int endIndex = 0;
+        
+        while(!complete){
+            if(data==null){System.out.println("EMPTY STRING");break;}
+            endIndex = data.indexOf(";", endIndex);
+            String substring = data.substring(beginIndex, endIndex);
+            beginIndex = endIndex;
+            
+            int commaIndex = substring.indexOf(",");
+            LeftMemory.addElement(Double.valueOf(substring.substring(0, commaIndex-1)));
+            RightMemory.addElement(Double.valueOf(substring.substring(commaIndex+1, substring.length())));
+        }
+        
+    }    
 }
