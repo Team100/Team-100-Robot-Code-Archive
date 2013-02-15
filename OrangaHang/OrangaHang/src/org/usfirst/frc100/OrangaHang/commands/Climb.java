1a89b2f6695478230e768148d3a4de11150e3f52
Sam Bunk <sambunk@comcast.net>
2013-02-15 10:12:36 -0800

    Updates to the autonomous system, it is not done yet Added the Camera widget extension

---
+++ OrangaHang/OrangaHang/src/org/usfirst/frc100/OrangaHang/commands/Climb.java
@@ -0,0 +1,62 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.usfirst.frc100.OrangaHang.commands;
+
+/**
+ *
+ * @author Team100
+ */
+public class Climb extends CommandBase {
+    
+    public Climb() {
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+        requires(climber);
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+        climber.resetLevel();
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+        while (!climber.getUpperLimit()){
+            climber.raiseElevator();
+        }
+        while(!climber.getLowerLimit()){
+            climber.lowerElevator();
+        }
+        climber.nextLevel();
+        while (!climber.getUpperLimit()){
+            climber.raiseElevator();
+        }
+        while(!climber.getLowerLimit()){
+            climber.lowerElevator();
+        }
+        climber.nextLevel();
+        while (!climber.getUpperLimit()){
+            climber.raiseElevator();
+        }
+        while(!climber.getPartwayLimit()){
+            climber.lowerElevatorPartway();
+        }
+        climber.nextLevel();
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return (climber.getLevel()>2);
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    }
+}
