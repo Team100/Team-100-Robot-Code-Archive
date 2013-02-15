34e0047e92afcc6ad310b2cd9e12ba87f1fd08f8
Paul <Paul@localhost>
2013-02-14 17:01:12 -0800

    Added intake code

--- OrangaHang/OrangaHang/src/org/usfirst/frc100/OrangaHang/subsystems/Intake.java
+++ OrangaHang/OrangaHang/src/org/usfirst/frc100/OrangaHang/subsystems/Intake.java
@@ -1,40 +1,58 @@
 package org.usfirst.frc100.OrangaHang.subsystems;
 
 import edu.wpi.first.wpilibj.DigitalInput;
-import edu.wpi.first.wpilibj.SpeedController;
+import edu.wpi.first.wpilibj.Victor;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import org.usfirst.frc100.OrangaHang.RobotMap;
 
 /**
  *
- * @author Team100
  */
 public class Intake extends Subsystem {
-    //Robot parts
-    private final SpeedController motor = RobotMap.intakeMotor;
-    private final DigitalInput topSwitch = RobotMap.intakeTopSwitch;
-    private final DigitalInput bottomSwitch = RobotMap.intakeBottomSwitch;
-    //Constants
-    private final double shootingSpeed = 0.5;
-    private final double intakeSpeed = -0.2;
-
+    DigitalInput intakeTopSwitch=RobotMap.intakeTopSwitch;
+    DigitalInput intakeBottomSwitch=RobotMap.intakeBottomSwitch;
+    Victor intakeMotor=RobotMap.intakeMotor; 
+    
+    //make these preferences!
+    double intakeSpeed=-.5;//negative
+    double shootingSpeed=.5;
+    
+    // Put methods for controlling this subsystem
+    // here. Call these from Commands.
+    
+    //empty
     public void initDefaultCommand() {
         // Set the default command for a subsystem here.
         //setDefaultCommand(new MySpecialCommand());
     }//end initDefaultCommand
-
-    public void shootFrisbees() {
-        if (topSwitch.get()) {
-            motor.set(shootingSpeed);
-        }
-    }//end shootFrisbees
-
-    public void takeFrisbees() {
-        if (!bottomSwitch.get()) {
-            motor.set(intakeSpeed);
-        } else {
-            motor.set(0.0);
+    
+    //call to load frisbees, does NOT run shooter wheels
+    public void takeFrisbees(){
+        if(!intakeBottomSwitch.get()){
+            intakeMotor.set(intakeSpeed);
         }
     }//end takeFrisbees
     
+    //slowly moves frisbees into shooter, does NOT run shooter wheels
+    public void shootFrisbees(){
+        if(!intakeTopSwitch.get()){
+            intakeMotor.set(shootingSpeed);
+        }
+    }//end shootFrisbees
+    
+    //sets motor to a given value, has safeties
+    public void manualControl(double d){
+        if (intakeTopSwitch.get()&&d>0||intakeBottomSwitch.get()&&d<0){
+            intakeMotor.set(0);
+        }
+        if (intakeTopSwitch.get()&&d<0){
+            intakeMotor.set(d);
+        }
+        if (intakeBottomSwitch.get()&&d>0){
+            intakeMotor.set(d);
+        }
+        if(!intakeTopSwitch.get()&&!intakeBottomSwitch.get()){
+            intakeMotor.set(d);
+        }
+    }//end manualControl
 }//end Intake
