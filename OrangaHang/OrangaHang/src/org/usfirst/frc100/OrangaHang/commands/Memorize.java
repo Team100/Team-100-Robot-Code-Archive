3de5e4792a9c6bfa3a3b4d33ca3a96e85b19a370
Sam Bunk <sambunk@comcast.net>
2013-02-16 10:55:22 -0800

    Implemented autonomous into main robot project Reproduce outputs are currently not going anywhere

--- OrangaHang/OrangaHang/src/org/usfirst/frc100/OrangaHang/commands/Memorize.java
+++ OrangaHang/OrangaHang/src/org/usfirst/frc100/OrangaHang/commands/Memorize.java
@@ -4,10 +4,51 @@
  */
 package org.usfirst.frc100.OrangaHang.commands;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import java.io.IOException;
+import org.usfirst.frc100.OrangaHang.OI;
+
 /**
  *
  * @author Sam
  */
-public class Memorize {
+public class Memorize extends CommandBase{
     
+    public Memorize(){
+        requires(autoMemory);
+    }
+
+    protected void initialize() {
+        System.out.println("Beginning Collection");
+        autoMemory.beginCollection();
+        SmartDashboard.putBoolean("Collecting", true);
+    }
+
+    /**
+     * Gets the current joystick values and sends them the autoMemory.collectString
+     * @see autoMemory
+     */
+    protected void execute() {
+        autoMemory.collectString(-OI.driverLeft.getY(), -OI.driverRight.getX(),
+                OI.shootButton.get(), OI.primeShootButton.get());
+        
+    } 
+
+    protected boolean isFinished() {
+        return false;
+    }
+
+    protected void interrupted() {
+        end();
+    }
+    
+    protected void end() {
+        try {
+            autoMemory.stopCollection();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        
+        SmartDashboard.putBoolean("Collecting", false);
+    } 
 }
