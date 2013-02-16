fd26f116425e0ba3a8af407d5925ede81999599e
Team100 <Team100@Cien.seq.org>
2013-02-16 08:11:23 -0800

    Commit to update.

---
+++ Sam/TeachMode/src/edu/wpi/first/wpilibj/templates/OI.java
@@ -0,0 +1,36 @@
+
+package edu.wpi.first.wpilibj.templates;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.templates.commands.Memorize;
+import edu.wpi.first.wpilibj.templates.commands.Reproduce;
+
+/**
+ * This class is the glue that binds the controls on the physical operator
+ * interface to the commands and command groups that allow control of the robot.
+ */
+public class OI {
+    
+    Joystick joystick = new Joystick(1);
+
+    public double getJoy1_y1() {
+        return joystick.getY();
+    }
+    
+    public double getJoy1_y2(){
+        return joystick.getThrottle();
+    }
+    
+    public OI(){
+        
+        //insert all triggered commands here
+        SmartDashboard.putData(new Memorize());
+        SmartDashboard.putData(new Reproduce());
+        
+    }
+    
+    public void init(){
+    }
+    
+}
