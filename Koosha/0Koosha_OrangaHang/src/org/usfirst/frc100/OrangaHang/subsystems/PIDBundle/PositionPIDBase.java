f16b969b30947f31fa24a518710859bee6f756a7
Isis Grant <Isis Grant@localhost>
2013-02-17 11:33:09 -0800

    Adding some more logic to climber, ShiftGears.

---
+++ Koosha/0Koosha_OrangaHang/src/org/usfirst/frc100/OrangaHang/subsystems/PIDBundle/PositionPIDBase.java
@@ -0,0 +1,160 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.usfirst.frc100.OrangaHang.subsystems.PIDBundle;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+/**
+ *
+ * @author Isis
+ */
+public class PositionPIDBase {
+    private boolean enabled = true;
+    private String name;
+    //Previous value variables
+    private double input = 0.0;
+    private double prevDist = 0.0; 
+    private double totalDistError = 0.0; 
+    private double output = 0.0;
+    private double filtDist = 0.0;
+    //Tuneables
+    private double kMaxVeloc = 1.0;
+    private double kMaxOutput = 0.5;
+    private double kMinOutput = 0.05;
+    private double kP = 0.0;
+    private double kI = 0.0;
+    private double kD = 0.0;
+    //Constants
+    private double setpoint = 0.0;
+    private double kDistRatio;
+    private double kDeadband = 0.05;
+
+    private void resetValues(){
+        input = 0.0;
+        prevDist = 0.0;
+        totalDistError = 0.0;
+        output = 0.0;
+        filtDist = 0.0;
+    }//end resetValues
+
+    public PositionPIDBase(double distRatio, String key){
+        kDistRatio = distRatio;
+        name = key;
+    }//end constructor
+
+    private String dashboardName(String key) {
+        return key + "_" + name;
+    }//end dashboardName
+    
+    public double calculate(double period){
+        if (!enabled) {
+            output = 0.0;
+            return output;
+        }
+
+        double goalDist = getSetpoint();
+
+        //calculate instantaneous velocity
+        double currDist = input / kDistRatio;
+        SmartDashboard.putNumber(dashboardName("currDist"), currDist);
+        double deltaDist = currDist - prevDist;
+        double instVeloc = deltaDist / period;
+        SmartDashboard.putNumber(dashboardName("instVeloc"), instVeloc);
+        
+        //rectangular motion profile
+        if (goalDist - filtDist > kMaxVeloc * period) {
+            filtDist += kMaxVeloc * period;
+        } else if (goalDist - filtDist < -kMaxVeloc * period) {
+            filtDist -= kMaxVeloc * period;
+        } else {
+            filtDist = goalDist;
+        }
+        double distError = filtDist - currDist;
+        totalDistError += distError / period;
+        SmartDashboard.putNumber(dashboardName("distError"), distError);
+        
+        //capping our integral
+        //don't increase totalDistError if kI is unset!
+        if (kI != 0.0){
+            double errorMax = kMaxOutput/kI;
+            if((errorMax < totalDistError) || (kI * totalDistError > kMaxOutput)){
+                totalDistError = errorMax;
+            }
+            if(-errorMax > totalDistError){
+                totalDistError = -errorMax;
+            }
+        } else if (kI == 0.0){
+            totalDistError = 0.0;
+        }        
+        
+        //compute the output
+        output = kP * distError + kI * totalDistError - kD*(instVeloc);
+
+        //limit to one direction of motion, eliminate deadband
+        if (output > 0.0) {
+            output = output + kDeadband;
+
+        } else if (output < 0.0) {
+            output = output - kDeadband;
+        }
+        if (output > kMaxOutput) {
+            output = kMaxOutput;
+        }
+        if (output < kMinOutput) {
+            output = kMinOutput;
+        }
+            
+        //setup for next loop
+        prevDist = currDist;
+
+        return output;
+    }// end calculate
+
+    
+    //obtain, set setpoint
+    public synchronized void setSetpoint(double sp){
+        setpoint = sp;
+    }//end setVelocSetpt
+    private synchronized double getSetpoint (){
+        return setpoint;
+    }//end getSetpoint
+
+    
+    //check enable/disable of robot
+    public void disable(){
+        enabled = false;
+    }//end disable
+    public void enable(){
+        if (!enabled){
+            resetValues();
+        }
+        enabled = true;
+    }//end enable
+
+    
+    //set tuneables
+    public void setInput(double put){
+        input = put;
+    }//end setInput
+    public void setKP(double p){
+        kP = p;
+    }//end setKP
+    public void setKI(double i){
+        kI = i;
+    }//end setKI
+    public void setKD(double d){
+        kD = d;
+    }//end setKD
+    public void setMaxOutput(double max){
+        kMaxOutput = max;
+    }//end setMaxOutput
+    public void setMinOutput(double min){
+        kMinOutput = min;
+    }//end setMaxOutput
+    public void setMaxVeloc(double vel){
+        kMaxVeloc = vel;
+    }//end setMaxVeloc
+
+}// end PositionPIDBase
---
+++ OrangaHang/OrangaHang/src/org/usfirst/frc100/OrangaHang/subsystems/DriveTrain.java
@@ -0,0 +1,148 @@
+package org.usfirst.frc100.OrangaHang.subsystems;
+
+import edu.wpi.first.wpilibj.*;
+import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import org.usfirst.frc100.OrangaHang.RobotMap;
+import org.usfirst.frc100.OrangaHang.commands.Drive;
+import org.usfirst.frc100.OrangaHang.subsystems.PIDBundle.PositionSendablePID;
+
+/**
+ *
+ * @author Team100
+ */
+public class DriveTrain extends Subsystem {
+    //Robot parts
+    private final SpeedController rightMotor = RobotMap.driveRightMotor;
+    private final SpeedController leftMotor = RobotMap.driveLeftMotor;
+    private final Encoder rightEncoder = RobotMap.driveRightEncoder;
+    private final Encoder leftEncoder = RobotMap.driveLeftEncoder;
+    private final Gyro gyro = RobotMap.driveGyro;
+    private final AnalogChannel ultraDist = RobotMap.driveUltrasonic;
+    private final DoubleSolenoid shifter = RobotMap.driveGear;
+    private final RobotDrive robotDrive=new RobotDrive(leftMotor, rightMotor);//add to robotMap?
+    //Constants
+    private final double kRightDistRatio = 1440 / ((4.0/12.0*3.14159));
+    private final double kLeftDistRatio = 1440 / ((4.0/12.0*3.14159));
+    //encoder ticks*(quadrature)/gearRatio*circumference*conversion to feet
+    private final double ultraDistRatio = 0.009794921875;
+    
+    public DriveTrain(){
+        leftEncoder.start();
+        rightEncoder.start();
+        robotDrive.setSafetyEnabled(false);
+    }
+    
+    //creates a new Drive
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        //setDefaultCommand(new MySpecialCommand());
+        setDefaultCommand(new Drive());
+    }//end initDefaultCommand
+    
+    //basic tankDrive
+    public void tankDrive(double leftSpeed, double rightSpeed){
+        leftMotor.set(leftSpeed);
+        rightMotor.set(rightSpeed);
+    }//end tankDrive
+    
+    //basic arcadeDrive: y=forward/backward speed, x=left/right speed
+    public void arcadeDrive(double y, double x){
+        robotDrive.arcadeDrive(y, x);
+    }// end arcadeDrive
+
+    public void shiftGears(){
+        if (!isHighGear()) {
+            //shifts gears to opposite position
+            shifter.set(DoubleSolenoid.Value.kForward);
+        } else if (isHighGear()) {
+            shifter.set(DoubleSolenoid.Value.kReverse);
+        }
+    }//end shiftGears
+    
+    public boolean isHighGear() {
+        return shifter.get().equals(DoubleSolenoid.Value.kForward);
+    }//end isHighGear
+    
+    //aligns robot for shooting
+    public void alignToShoot(double left, double right) {
+        if (ultraDist.getVoltage() < 1.2) {
+            if (left > 0) {
+                leftMotor.set(0);
+                rightMotor.set(0);
+            } else {
+                arcadeDrive(left, right);
+            }
+        } else {
+            arcadeDrive(left, right);
+        }
+        
+//        if(Math.abs(gyro.getAngle()) > 2) {
+//            leftMotor.set(gyro.getAngle() / 20);
+//            rightMotor.set(gyro.getAngle() / -20);
+//        } else {
+//            if(ultraDist.getVoltage() > 1.0) {
+//                leftMotor.set(1.0);
+//                rightMotor.set(-1.0);
+//            } else {
+//                leftMotor.set(0);
+//                rightMotor.set(0);
+//            }
+//        }
+        
+    }//end alignToShoot
+    
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    
+    //PID Control
+    //driveRight
+    PIDSource sourceRight = new PIDSource(){
+        public double pidGet(){
+            SmartDashboard.putNumber("encoderRight_raw", rightEncoder.getRaw());
+            return rightEncoder.getRaw();
+        }
+    }; //end anonym class PIDSource
+    PIDOutput outputRight = new PIDOutput(){
+        public void pidWrite(double output){
+            rightMotor.set(output);
+        }
+    }; //end anonym class PIDOutput
+    private PositionSendablePID pidRight = new PositionSendablePID("right",sourceRight, outputRight, kRightDistRatio);
+    
+    //driveLeft
+    PIDSource sourceLeft = new PIDSource(){
+        public double pidGet(){
+            SmartDashboard.putNumber("encoderLeft_raw", leftEncoder.get());
+            return leftEncoder.get();
+        }
+    }; //end anonym class PIDSource
+    PIDOutput outputLeft = new PIDOutput(){
+        public void pidWrite(double output){
+             leftMotor.set(output);
+        }
+    }; //end anonym class PIDOutput
+    private PositionSendablePID pidLeft = new PositionSendablePID("left",sourceLeft, outputLeft, kLeftDistRatio);
+    
+    public void setSetpoint(double setpoint){
+        pidRight.setSetpoint(setpoint);
+        pidLeft.setSetpoint(setpoint);
+    }//end setSetpoint
+    
+    public void disable(){
+        setSetpoint(0.0);
+        pidRight.disable();
+        pidLeft.disable();
+        rightEncoder.reset();
+        leftEncoder.reset();
+    }//end disable
+    
+    public void enable(){
+        rightEncoder.reset();
+        leftEncoder.reset();
+        rightEncoder.start();
+        leftEncoder.start();
+        pidRight.enable();
+        pidLeft.enable();
+    }//end enable
+    
+}//end DriveTrain
