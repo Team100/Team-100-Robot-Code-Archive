a88c3cf2751206fa6fa8c92964cc860fd1ee269d
Student <Student@Student-PC.seq.org>
2013-02-17 11:58:57 -0800

    Added manualTilt and titToStart, remapped operator controls.

---
+++ Koosha/StringWidget/src/team0100/smartdashboard/extension/Widget/Widget.java
@@ -0,0 +1,64 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package team0100.smartdashboard.extension.Widget;
+
+import edu.wpi.first.smartdashboard.gui.StaticWidget;
+import edu.wpi.first.smartdashboard.properties.Property;
+import edu.wpi.first.wpilibj.networktables.NetworkTable;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+
+/**
+ *
+ * @author Student
+ */
+public class Widget extends StaticWidget
+{
+    public static final String NAME = "StringWidget";
+    String temp = "One,Two,Three,Four";
+    private String[] buttonNames = this.split(temp);
+    
+        
+    NetworkTable table = NetworkTable.getTable("SmartDashboard");
+    
+    @Override
+    public void init()
+    {
+        JButton[] buttons = null;
+        for(int i=0; i<buttonNames.length; i++)
+        {
+            final JButton tempbutton = new JButton(buttonNames[i]);
+            
+            buttons[i] = new JButton(buttonNames[i]); 
+            add(buttons[i], i);
+            buttons[i].setFocusable(false);
+            buttons[i].addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent ae)
+                {
+                    //System.out.println(tempButton.getName());
+                }
+            });
+        }
+    }
+
+    @Override
+    public void propertyChanged(Property prprt)
+    {
+        
+    }
+    
+    public String[] split(String patient)
+    {
+        String[] returnVal;
+        returnVal = patient.split(",");
+        for(int i=0; i<returnVal.length; i++)
+        {
+            System.out.println(returnVal[i]);
+        }
+        return returnVal;
+    }
+}
