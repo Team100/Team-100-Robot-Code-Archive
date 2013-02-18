3143b9ddcc868399a4e61be1a3a297ab35f28047
Daniel <DBishop.rwc@gmail.com>
2013-02-18 10:32:32 -0800

    Pidget bugfixes

---
+++ OrangaHang/OrangaHang/src/org/usfirst/frc100/OrangaHang/subsystems/Shifter.java
@@ -0,0 +1,48 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.usfirst.frc100.OrangaHang.subsystems;
+
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.command.Subsystem;
+import org.usfirst.frc100.OrangaHang.RobotMap;
+
+/**
+ *
+ * @author Student
+ */
+public class Shifter extends Subsystem {
+    private final DoubleSolenoid shifter = RobotMap.driveGear;
+
+    // Put methods for controlling this subsystem
+    // here. Call these from Commands.
+
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        //setDefaultCommand(new MySpecialCommand());
+        
+        
+    }
+    
+    public void shiftHighGear()
+    {
+        if(!isHighGear())
+        {
+            shifter.set(DoubleSolenoid.Value.kForward);
+        }
+    }
+    
+    public void shiftLowGear()
+    {
+        if(isHighGear())
+        {
+            shifter.set(DoubleSolenoid.Value.kReverse);
+        }
+    }
+    
+    public boolean isHighGear()
+    {
+        return shifter.get().equals(DoubleSolenoid.Value.kForward);
+    }
+}
---
+++ OrangaHang/OrangaHang/src/org/usfirst/frc100/OrangaHang/subsystems/Tower.java
@@ -0,0 +1,120 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.usfirst.frc100.OrangaHang.subsystems;
+
+import edu.wpi.first.wpilibj.AnalogChannel;
+import edu.wpi.first.wpilibj.PIDOutput;
+import edu.wpi.first.wpilibj.PIDSource;
+import edu.wpi.first.wpilibj.Victor;
+import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import org.usfirst.frc100.OrangaHang.RobotMap;
+import org.usfirst.frc100.OrangaHang.commands.TestTilter;
+import org.usfirst.frc100.OrangaHang.subsystems.PIDBundle.PositionSendablePID;
+
+/**
+ *
+ * @author Team100
+ */
+public class Tower extends Subsystem
+{
+    //Robot parts
+    private final AnalogChannel potentiometer = RobotMap.towerPotent;
+    private final Victor motor = RobotMap.towerMotor;
+    //Constants
+    private double kClimbPosition = 60.0;
+    private double kShootPosition = 50.0;
+    private double kIntakePosition = 60.0;
+    private double kStartPosition = 95.0;
+    private double kTowerAngleRatio = 0.277;
+    private double kZeroAngle = 228;
+
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        //setDefaultCommand(new MySpecialCommand());
+        setDefaultCommand(new TestTilter());
+    }//end initDefaultCommand
+    
+    public void manualControl(double s)
+    {
+        if(potentiometer.getValue()>=68.0 && s>=0)
+        {
+            motor.set(s);
+        }
+        else if(potentiometer.getValue()<=0.0 && s<0)
+        {
+            motor.set(s);
+        }
+    }
+    
+    //calculates and returns the angle of the tower
+    public double getAngle(){
+        return kZeroAngle-potentiometer.getValue()*kTowerAngleRatio;
+    }//end getAngle()
+    
+    public void testTilter(double speed){
+        double pot=getAngle();
+        int max=90;
+        int min=60;
+        if (pot<max&&speed>0||pot>min&&speed<0){
+            motor.set(speed);
+        }
+        if (pot<min&&speed<0){
+            motor.set(0);        
+        }
+        if (pot>max&&speed>0){
+            motor.set(0);
+        }
+        if(pot<max&&pot>min){
+            motor.set(speed);        
+        }
+    }
+
+    //PID control
+    PIDSource sourceTower = new PIDSource() {
+        public double pidGet() {
+            SmartDashboard.putNumber("potVoltage_raw", potentiometer.getValue());
+            return potentiometer.getValue();
+        }
+    }; //end anonym class PIDSource
+    PIDOutput outputTower = new PIDOutput() {
+        public void pidWrite(double output) {
+            motor.set(output);
+        }
+    }; //end anonym class PIDOutput
+    private PositionSendablePID pidTower = new PositionSendablePID("Tower", sourceTower, outputTower, kTowerAngleRatio);
+
+    public void setSetpoint(double setpoint) {
+        pidTower.setSetpoint(setpoint);
+    }//end setSetpoint
+
+    public void tiltToClimb() {
+        enable();
+        pidTower.setSetpoint(kClimbPosition);
+    }//end tiltToClimb
+
+    public void tiltToShoot() {
+        enable();
+        pidTower.setSetpoint(kShootPosition);
+    }//end tiltToClimb
+
+    public void tiltToIntake() {
+        enable();
+       pidTower.setSetpoint(kIntakePosition);
+    }//end tiltToIntake
+    
+    public void tiltToStart(){
+        enable();
+       pidTower.setSetpoint(kStartPosition);
+    }//end tiltToStart
+
+    public void disable() {
+        pidTower.disable();
+    }//end disable
+
+    public void enable() {
+        pidTower.enable();
+    }//end enable    
+}//end Tower
