f64e9df16646b47d16d4e29ed250aa38f3007a70
Daniel <DBishop.rwc@gmail.com>
2013-02-18 09:36:34 -0800

    Update to PID Widget 2.0, added source code

---
+++ OrangaHang/OrangaHang/src/org/usfirst/frc100/OrangaHang/commands/FullManualControl.java
@@ -0,0 +1,120 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.usfirst.frc100.OrangaHang.commands;
+
+import org.usfirst.frc100.OrangaHang.OI;
+
+/**
+ *
+ * @author Team100
+ */
+public class FullManualControl extends CommandBase {
+    
+    public FullManualControl()
+    {
+        // Use requires() here to declare subsystem dependencies
+        requires(climber);
+        requires(shooter);
+        requires(driveTrain);
+        requires(intake);
+        requires(tower);
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize()
+    {
+        
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute()
+    {
+        double speed = OI.driverLeft.getY();
+        
+        if(OI.tiltClimbButton.get())
+        {
+            driveTrain.setLeftMotor(speed);
+        }
+        
+        if(OI.tiltShootButton.get())
+        {
+            driveTrain.setRightMotor(speed);
+        }
+        
+        if(OI.tiltIntakeButton.get())
+        {
+            climber.manualControl(speed);
+        }
+        
+        if(OI.primeShootButton.get())
+        {
+            shooter.setFrontMotor(speed);
+        }
+        
+        if(OI.shootButton.get())
+        {
+            shooter.setBackMotor(speed);
+        }
+        
+        if(OI.primeDumpButton.get())
+        {
+            //intake.manualControl(speed);
+        }
+        
+        if(OI.intakeButton.get())
+        {
+            tower.manualControl(speed);
+        }
+        
+        if(OI.manipulator.getRawAxis(6)<0.0)
+        {
+            driveTrain.shiftHighGear();
+        }
+        
+        if(OI.manipulator.getRawAxis(6)>0.0)
+        {
+            driveTrain.shiftLowGear();
+        }
+        
+        if(OI.manipulator.getRawAxis(5)<0.0 && !tower.isStowed())
+        {
+            tower.toggleArms();
+        }
+        
+        if(OI.manipulator.getRawAxis(5)>0.0 && tower.isStowed())
+        {
+            tower.toggleArms();
+        }
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return false;
+    }
+
+    // Called once after isFinished returns true
+    protected void end()
+    {
+        driveTrain.setLeftMotor(0.0);
+        driveTrain.setRightMotor(0.0);
+        climber.disable();
+        shooter.setFrontMotor(0.0);
+        shooter.setBackMotor(0.0);
+        //intake.setMotor(0.0);
+        tower.disable();
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted()
+    {
+        driveTrain.setLeftMotor(0.0);
+        driveTrain.setRightMotor(0.0);
+        climber.disable();
+        shooter.setFrontMotor(0.0);
+        shooter.setBackMotor(0.0);
+        tower.disable();
+    }
+}
---
+++ OrangaHang/OrangaHang/src/org/usfirst/frc100/OrangaHang/commands/ToggleArms.java
@@ -0,0 +1,46 @@
+
+//ToggleArms moves the fixed (pivoting) arms into the opposite position. 
+
+
+package org.usfirst.frc100.OrangaHang.commands;
+
+import org.usfirst.frc100.OrangaHang.OI;
+
+/**
+ *
+ * @author Team100
+ */
+public class ToggleArms extends CommandBase {
+    public ToggleArms() {
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+        //requires(tower);
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+        tower.toggleArms();
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return true;
+    }
+
+    // Called once after isFinished returns true
+    protected void end()
+    {
+        
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted()
+    {
+        
+    }
+}
