56ef92dc34da02f7a4c55fb0caeb5f3332d3a75f
Student <Student@Student-PC.seq.org>
2013-02-18 14:08:19 -0800

    Working with tower tilt.

--- OrangaHang/OrangaHang/src/org/usfirst/frc100/OrangaHang/commands/TestTilter.java
+++ OrangaHang/OrangaHang/src/org/usfirst/frc100/OrangaHang/commands/TestTilter.java
@@ -24,7 +24,7 @@ public class TestTilter extends CommandBase {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-        tower.testTilter(OI.manipulator.getThrottle()/3);
+        tower.testTilter(OI.manipulator.getThrottle());
     }
 
     // Make this return true when this Command no longer needs to run execute()
