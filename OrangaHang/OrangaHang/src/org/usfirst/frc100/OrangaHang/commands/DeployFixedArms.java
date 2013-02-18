69af8c785844e3a21f55f62fc8fe4002e5b3e2e0
Koosha Jahani <Koosha Jahani@localhost>
2013-02-17 17:47:30 -0800

    added all my pneumatic and compressor code, my full manual control code, and quickTurn code

--- OrangaHang/OrangaHang/src/org/usfirst/frc100/OrangaHang/commands/DeployFixedArms.java
+++ OrangaHang/OrangaHang/src/org/usfirst/frc100/OrangaHang/commands/DeployFixedArms.java
@@ -4,38 +4,49 @@
  */
 package org.usfirst.frc100.OrangaHang.commands;
 
+import org.usfirst.frc100.OrangaHang.OI;
+
 /**
  *
  * @author Team100
  */
-public class DeployFixedArms extends CommandBase {
-    
-    public DeployFixedArms() {
+public class DeployFixedArms extends CommandBase
+{    
+    public DeployFixedArms()
+    {
         // Use requires() here to declare subsystem dependencies
         // eg. requires(chassis);
         //requires(tower);
     }
 
     // Called just before this Command runs the first time
-    protected void initialize() {
+    protected void initialize()
+    {
+        tower.deployArms();
     }
 
     // Called repeatedly when this Command is scheduled to run
-    protected void execute() {
-        tower.toggleArms();
+    protected void execute()
+    {
+        
     }
 
     // Make this return true when this Command no longer needs to run execute()
-    protected boolean isFinished() {
-        return true;
+    protected boolean isFinished()
+    {
+        return !OI.deployArmsButton.get();
     }
 
     // Called once after isFinished returns true
-    protected void end() {
+    protected void end()
+    {
+        
     }
 
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
-    protected void interrupted() {
+    protected void interrupted()
+    {
+        
     }
 }
