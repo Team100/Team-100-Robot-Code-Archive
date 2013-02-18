091ead76c78a82bec6510f540f042f11099674c9
Student <Student@Student-PC.seq.org>
2013-02-18 10:02:24 -0800

    Changed tower command structure.

---
+++ OrangaHang/OrangaHang/src/org/usfirst/frc100/OrangaHang/RobotMap.java
@@ -0,0 +1,147 @@
+package org.usfirst.frc100.OrangaHang;
+
+import edu.wpi.first.wpilibj.*;
+import edu.wpi.first.wpilibj.livewindow.LiveWindow;
+
+/**
+ * The RobotMap is a mapping from the ports sensors and actuators are wired into
+ * to a variable name. This provides flexibility changing wiring, makes checking
+ * the wiring easier and significantly reduces the number of magic numbers
+ * floating around.
+ */
+public class RobotMap {
+    // For example to map the left and right motors, you could define the
+    // following variables to use with your drivetrain subsystem.
+    // public static final int leftMotor = 1;
+    // public static final int rightMotor = 2;
+    
+    // If you are using multiple modules, make sure to define both the port
+    // number and the module. For example you with a rangefinder:
+    // public static final int rangefinderPort = 1;
+    // public static final int rangefinderModule = 1;
+    
+    //Digital I/O sensors
+    //Drive Train
+    public static final Encoder driveRightEncoder = new Encoder(3,4);
+    public static final Encoder driveLeftEncoder = new Encoder(5,6);
+    //Climber
+    public static final Encoder climberEncoder = new Encoder(7,8);
+    public static final DigitalInput climberTopSwitch = new DigitalInput(9);
+    public static final DigitalInput climberBottomSwitch = new DigitalInput(10);
+    public static final DigitalInput climberPoleSwitch = new DigitalInput(13);  
+    //Shooter
+    public static final DigitalInput shooterFrontHallEffect = new DigitalInput(1);
+    public static final DigitalInput shooterBackHallEffect = new DigitalInput(2);
+    //Intake
+    public static final DigitalInput intakeTopSwitch = new DigitalInput(11);
+    public static final DigitalInput intakeBottomSwitch = new DigitalInput(12);
+    //add pressure switch?
+    
+    //Analog sensors
+    //Drive Train
+    public static final Gyro driveGyro = new Gyro(1);
+    public static final AnalogChannel driveUltrasonic = new AnalogChannel(2);
+    //Tower
+    public static final AnalogChannel towerPotent = new AnalogChannel(3);
+    //Climber
+    public static final AnalogChannel climberRightFixedIR = new AnalogChannel(4);
+    public static final AnalogChannel climberLeftFixedIR = new AnalogChannel(5);
+    public static final AnalogChannel climberRightMovingIR = new AnalogChannel(6);
+    public static final AnalogChannel climberLeftMovingIR = new AnalogChannel(7);
+    
+    
+    //PWM Outputs
+    //Drive Train
+    public static final Talon driveLeftMotor = new Talon(2);
+    public static final Talon driveRightMotor = new Talon(1);
+    //Climber
+    public static final Victor climberTopMotor = new Victor(3);
+    public static final Victor climberBottomMotor = new Victor(4);
+    //Shooter
+    public static final Victor shooterFrontMotor = new Victor(5);
+    public static final Victor shooterBackMotor = new Victor(6);
+    //Intake
+    public static final Victor intakeMotor = new Victor(8);
+    //Tower
+    public static final Victor towerMotor = new Victor(7);
+    
+    
+    //Solenoids
+    //Drive Train
+    public static final DoubleSolenoid driveGear = new DoubleSolenoid(1,2);
+    //Tower
+    public static final DoubleSolenoid towerArmPistons = new DoubleSolenoid(3,4);
+    //Relays
+    public static final Compressor compressor = new Compressor(14,1);
+    public static final Relay cameraLights = new Relay(2);
+    
+    //LiveWindow code
+    public static void init(){
+        //LiveWindow display
+        //Drive Train
+        LiveWindow.addSensor("DriveTrain", "RightEncoder", driveRightEncoder);
+        LiveWindow.addSensor("DriveTrain", "LeftEncoder", driveLeftEncoder);
+        LiveWindow.addActuator("DriveTrain", "LeftMotor2", driveLeftMotor);
+        LiveWindow.addActuator("DriveTrain", "RightMotor1", driveRightMotor);
+        LiveWindow.addSensor("DriveTrain", "Gyro" , driveGyro);
+        LiveWindow.addSensor("DriveTrain", "Ultrasonic" , driveUltrasonic);
+        LiveWindow.addActuator("DriveTrain", "Gear" , driveGear);
+        
+        //Climber
+        LiveWindow.addSensor("Climber", "ClimberEncoder" , climberEncoder);
+        LiveWindow.addSensor("Climber", "TopSwitch" , climberTopSwitch);
+        LiveWindow.addSensor("Climber", "BottomSwitch" , climberBottomSwitch);
+        LiveWindow.addSensor("Climber", "PoleSwitch" , climberPoleSwitch);
+        LiveWindow.addActuator("Climber", "TopMotor3", climberTopMotor);
+        LiveWindow.addActuator("Climber", "BottomMotor4", climberBottomMotor);
+        LiveWindow.addSensor("Climber", "RightFixedIR" , climberRightFixedIR);
+        LiveWindow.addSensor("Climber", "RightMovingIR" , climberRightMovingIR);
+        LiveWindow.addSensor("Climber", "LeftFixedIR" , climberLeftFixedIR);
+        LiveWindow.addSensor("Climber", "LeftMovingIR" , climberLeftMovingIR);
+        
+        //Shooter
+        LiveWindow.addActuator("Shooter", "FrontMotor5", shooterFrontMotor);
+        LiveWindow.addActuator("Shooter", "BackMotor6", shooterBackMotor);
+        LiveWindow.addSensor("Shooter", "FrontHallEffect" , shooterFrontHallEffect);
+        LiveWindow.addSensor("Shooter", "BackHallEffect" , shooterBackHallEffect);
+        
+        //Intake
+        LiveWindow.addActuator("Intake", "Motor8", intakeMotor);
+        LiveWindow.addSensor("Intake", "TopSwitch" , intakeTopSwitch);
+        LiveWindow.addSensor("Intake", "BottomSwitch" , intakeBottomSwitch);
+        
+        //Tower
+        LiveWindow.addActuator("Tower", "Motor7", towerMotor);
+        LiveWindow.addSensor("Tower", "Potent" , towerPotent);
+        LiveWindow.addActuator("Tower", "ArmPistons" , towerArmPistons);
+        
+        //Relays
+        LiveWindow.addActuator("Relays", "Compressor" , compressor);
+        LiveWindow.addActuator("Relays", "CameraLights" , cameraLights);        
+        
+    }//end init
+
+    //puts safeties on all motors
+    public static void safe() {
+        driveLeftMotor.setSafetyEnabled(true);
+        driveRightMotor.setSafetyEnabled(true);
+        climberTopMotor.setSafetyEnabled(true);
+        climberBottomMotor.setSafetyEnabled(true);
+        shooterFrontMotor.setSafetyEnabled(true);
+        shooterBackMotor.setSafetyEnabled(true);
+        intakeMotor.setSafetyEnabled(true);
+        towerMotor.setSafetyEnabled(true);
+    }//end safe
+
+    //removes safeties from all motors (for LiveWindow)
+    public static void unSafe() {
+        driveLeftMotor.setSafetyEnabled(false);
+        driveRightMotor.setSafetyEnabled(false);
+        climberTopMotor.setSafetyEnabled(false);
+        climberBottomMotor.setSafetyEnabled(false);
+        shooterFrontMotor.setSafetyEnabled(false);
+        shooterBackMotor.setSafetyEnabled(false);
+        intakeMotor.setSafetyEnabled(false);
+        towerMotor.setSafetyEnabled(false);
+    }//end unSafe
+}//end RobotMap
---
+++ OrangaHang/OrangaHang/src/org/usfirst/frc100/OrangaHang/subsystems/DriveTrain.java
@@ -0,0 +1,189 @@
+package org.usfirst.frc100.OrangaHang.subsystems;
+
+import edu.wpi.first.wpilibj.*;
+import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import org.usfirst.frc100.OrangaHang.RobotMap;
+import org.usfirst.frc100.OrangaHang.commands.CommandBase;
+import org.usfirst.frc100.OrangaHang.commands.Drive;
+import org.usfirst.frc100.OrangaHang.subsystems.PIDBundle.PositionSendablePID;
+
+/**
+ *
+ * @author Team100
+ */
+public class DriveTrain extends Subsystem {
+    //Robot parts
+    private final SpeedController rightMotor = RobotMap.driveRightMotor;
+    private final SpeedController leftMotor = RobotMap.driveLeftMotor;
+    private final Encoder rightEncoder = RobotMap.driveRightEncoder;
+    private final Encoder leftEncoder = RobotMap.driveLeftEncoder;
+    private final Gyro gyro = RobotMap.driveGyro;
+    private final AnalogChannel ultraDist = RobotMap.driveUltrasonic;
+    private final RobotDrive robotDrive=new RobotDrive(leftMotor, rightMotor);//add to robotMap?
+    //Constants
+    //circumference/ticks
+    private final double kRightDistRatio = ((4.0/12.0*Math.PI))/1440;
+    private final double kLeftDistRatio = ((4.0/12.0*Math.PI))/1440;
+    private final double ultraDistRatio = 0.009794921875;
+    private double setpoint;
+    
+    //starts encoders
+    public DriveTrain(){
+        leftEncoder.start();
+        rightEncoder.start();
+        gyro.reset();
+    }//end constructor
+    
+    //removes safeties from the robotDrive
+    public void unSafe(){
+        robotDrive.setSafetyEnabled(false);
+    }//end unSafe
+    
+    //puts safeties on the robotDrive
+    public void safe(){
+        robotDrive.setSafetyEnabled(true);
+    }//end safe
+    
+    //creates a new Drive
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        //setDefaultCommand(new MySpecialCommand());
+        setDefaultCommand(new Drive());
+    }//end initDefaultCommand
+    
+    public void setLeftMotor(double s)
+    {
+        leftMotor.set(s);
+    }
+    
+    public void setRightMotor(double s)
+    {
+        rightMotor.set(s);
+    }
+    
+    public void tankDrive(double leftSpeed, double rightSpeed){
+        leftMotor.set(leftSpeed);
+        rightMotor.set(rightSpeed);
+    }//end tankDrive
+    
+    //basic arcadeDrive: y=forward/backward speed, x=left/right speed
+    public void arcadeDrive(double y, double x){
+        robotDrive.arcadeDrive(y, x);
+        SmartDashboard.putNumber("LEFT ENCODER", leftEncoder.get());
+        SmartDashboard.putNumber("RIGHT ENCODER", rightEncoder.get());
+        SmartDashboard.putNumber("GYRO", gyro.getAngle());
+    }// end arcadeDrive
+    
+    public void resetGyro()
+    {
+        gyro.reset();
+    }
+
+    public double getGyro()
+    {
+        return gyro.getAngle();
+    }
+    
+    //aligns robot for shooting
+    public void alignToShoot(double left, double right){
+        
+        if(ultraDist.getVoltage() < 1.2) {
+            if(left > 0) {
+                leftMotor.set(0);
+                rightMotor.set(0);
+            } else {
+                arcadeDrive(left, right);
+            }
+        } else {
+            arcadeDrive(left, right);
+        }
+        
+//        if(Math.abs(gyro.getAngle()) > 2) {
+//            leftMotor.set(gyro.getAngle() / 20);
+//            rightMotor.set(gyro.getAngle() / -20);
+//        } else {
+//            if(ultraDist.getVoltage() > 1.0) {
+//                leftMotor.set(1.0);
+//                rightMotor.set(-1.0);
+//            } else {
+//                leftMotor.set(0);
+//                rightMotor.set(0);
+//            }
+//        }
+        
+    }//end alignToShoot
+    
+    public void quickTurn(double angle){
+        if (Math.abs(gyro.getAngle()-angle)>.8){
+            leftMotor.set(angle/90);
+            rightMotor.set(angle/90);
+        }
+    }
+    
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    //FIXME: remove PID code if we don't use it
+    //PID Control
+    //driveRight
+    PIDSource sourceRight = new PIDSource(){
+        public double pidGet(){
+            SmartDashboard.putNumber("encoderRight_raw", rightEncoder.getRaw());
+            return rightEncoder.getRaw();
+        }
+    }; //end anonym class PIDSource
+    PIDOutput outputRight = new PIDOutput(){
+        public void pidWrite(double output){
+            rightMotor.set(output);
+        }
+    }; //end anonym class PIDOutput
+//    private PositionSendablePID pidRight = new PositionSendablePID("right",sourceRight, outputRight, kRightDistRatio);
+    
+    //driveLeft
+    PIDSource sourceLeft = new PIDSource(){
+        public double pidGet(){
+            SmartDashboard.putNumber("encoderLeft_raw", leftEncoder.get());
+            return leftEncoder.get();
+        }
+    }; //end anonym class PIDSource
+    PIDOutput outputLeft = new PIDOutput(){
+        public void pidWrite(double output){
+             leftMotor.set(output);
+        }
+    }; //end anonym class PIDOutput
+//    private PositionSendablePID pidLeft = new PositionSendablePID("left",sourceLeft, outputLeft, kLeftDistRatio);
+    
+    public void setSetpoint(double setpoint){
+        this.setpoint = setpoint;
+//        pidRight.setSetpoint(setpoint);
+//        pidLeft.setSetpoint(setpoint);
+    }//end setSetpoint
+    
+    public double getSetpoint()
+    {
+        return this.setpoint;
+    }
+    
+    public void disable(){
+        setSetpoint(0.0);
+ //       pidRight.disable();
+ //       pidLeft.disable();
+        rightEncoder.reset();
+        leftEncoder.reset();
+    }//end disable
+    
+    public void enable(){
+        rightEncoder.reset();
+        leftEncoder.reset();
+        rightEncoder.start();
+        leftEncoder.start();
+ //       pidRight.enable();
+ //       pidLeft.enable();
+    }//end enable
+    public void resetValues()
+    {
+//        pidRight.getValues(); // Extremly misleading name; doesn't return anything. Resets all constants
+//        pidLeft.getValues();
+        //pidTurn.getValues();
+    }
+    
+}//end DriveTrain
