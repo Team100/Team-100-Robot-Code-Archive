e579b5903d110fe5ba74e2363d7bc59242d24fae
Isis <Isis@localhost>
2013-03-06 18:04:56 -0800

    Fixing AlignToShoot, calibrating rangefinder for drivetrain, speeding up dump speed and frisbee transport speed for shooting.

---
+++ OrangaHang/OrangaHang/src/org/usfirst/frc100/OrangaHang/commands/Memorize.java
@@ -0,0 +1,51 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.usfirst.frc100.OrangaHang.commands;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import java.io.IOException;
+import org.usfirst.frc100.OrangaHang.OI;
+
+/**
+ *
+ * @author Sam
+ */
+public class Memorize extends CommandBase{
+    
+    public Memorize(){
+        requires(autoMemory);
+    }
+
+    protected void initialize() {
+        autoMemory.beginCollection();
+    }
+
+    /**
+     * Gets the current joystick values and sends them the autoMemory.collectString
+     * @see autoMemory
+     */
+    protected void execute() {
+        autoMemory.collectString(-OI.driverLeft.getY(), -OI.driverRight.getX(),
+                OI.shootButton.get(), OI.primeShootButton.get());
+        
+    } 
+
+    protected boolean isFinished() {
+        //TODO: run for 15 seconds
+        return false;
+    }
+
+    protected void end() {
+        try {
+            autoMemory.stopCollection();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }        
+    }
+    
+    protected void interrupted() {
+        end();
+    }    
+}
