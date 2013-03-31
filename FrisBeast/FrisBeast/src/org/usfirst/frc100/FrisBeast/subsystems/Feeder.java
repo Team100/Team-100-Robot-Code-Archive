e34eff99bf9cfce70778ebe75e6275ef6d2e0795
Team100 <Team100@Cien>
2013-03-31 10:56:35 -0700

    Added hardcoded autonomous, adjustible delays for shooting in both teleop & auton.

--- FrisBeast/FrisBeast/src/org/usfirst/frc100/FrisBeast/subsystems/Feeder.java
+++ FrisBeast/FrisBeast/src/org/usfirst/frc100/FrisBeast/subsystems/Feeder.java
@@ -5,6 +5,7 @@
 package org.usfirst.frc100.FrisBeast.subsystems;
 
 import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.Preferences;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import org.usfirst.frc100.FrisBeast.RobotMap;
 
@@ -14,11 +15,25 @@ import org.usfirst.frc100.FrisBeast.RobotMap;
  */
 public class Feeder extends Subsystem implements SubsystemControl {
     private final DoubleSolenoid feederPistons = RobotMap.feederPistons;
+    private final double kDefaultShootDelay = 0.5;
+    
+    public Feeder(){
+        Preferences p = Preferences.getInstance();
+        if(!p.containsKey("FeederShootDelay")) {
+            p.putDouble("FeederShootDelay", kDefaultShootDelay);
+        }
+    }//end constructor
     
     public void initDefaultCommand() {
         // Set the default command for a subsystem here.
         //setDefaultCommand(new MySpecialCommand());
     }//end initDefaultCommand
+    
+    public double getShootDelay(){
+        Preferences p = Preferences.getInstance();
+        final double kShootDelay = p.getDouble("FeederShootDelay", kDefaultShootDelay);
+        return kShootDelay;
+    }//end getShootDelay
     
     public void pushForward(){
         feederPistons.set(DoubleSolenoid.Value.kForward);
