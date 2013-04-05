5fc79ea4136e09a6a79310bd4346e9b451a17229
Team100 <Team100@Cien>
2013-04-04 17:30:40 -0700

    Extended PrimeHighSpeed time limit to 15s, to span full autonomous period.

--- FrisBeast/FrisBeast/src/org/usfirst/frc100/FrisBeast/commands/PrimeHighSpeed.java
+++ FrisBeast/FrisBeast/src/org/usfirst/frc100/FrisBeast/commands/PrimeHighSpeed.java
@@ -27,7 +27,7 @@ public class PrimeHighSpeed extends CommandBase {
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return this.timeSinceInitialized()>10;//whileHeld
+        return this.timeSinceInitialized()>15;//whileHeld
     }
 
     // Called once after isFinished returns true
