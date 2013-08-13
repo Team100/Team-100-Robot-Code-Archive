08f21c697c9cc0efc48d13235adf1d2c21d52fe7
Paul <Paul@localhost>
2013-08-13 14:10:43 -0700

    Autonomous incorporated into Mk3.

--- Mk3/Mk3/src/org/usfirst/frc100/Mk3/commands/Autonomous.java
+++ Mk3/Mk3/src/org/usfirst/frc100/Mk3/commands/Autonomous.java
@@ -1,36 +1,122 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package org.usfirst.frc100.Mk3.commands;
 
-import edu.wpi.first.wpilibj.command.CommandGroup;
+import edu.wpi.first.wpilibj.Preferences;
 
 /**
- *
  * @author Team100
  */
-public class Autonomous extends CommandGroup {
-    
-    public Autonomous(double initialDelay, double timeout) {
-        // Add Commands here:
-        // e.g. addSequential(new Command1());
-        //      addSequential(new Command2());
-        // these will run in order.
 
-        // To run multiple commands at the same time,
-        // use addParallel()
-        // e.g. addParallel(new Command1());
-        //      addSequential(new Command2());
-        // Command1 and Command2 will run in parallel.
+public class Autonomous extends CommandBase {
 
-        // A command group will require all of the subsystems that each member
-        // would require.
-        // e.g. if Command1 requires chassis, and Command2 requires arm,
-        // a CommandGroup containing them would require both the chassis and the
-        // arm.
-        //addParallel(new PrimeHighSpeed(), timeout);//second parameter is timeout. Stops shooter wheels so we don't kill the battery
-        //addSequential(new Shoot());
-    }//end constructor
+    int state = 0;
+    int shotNumber = 0;
+    Preferences pref = Preferences.getInstance();
     
+    public Autonomous() {
+        requires(driveTrain);
+        requires(shooter);
+        requires(intake);
+    }
+
+    protected void initialize() {
+        intake.runIntake();
+    }
+
+    protected void execute() {
+        intake.tiltToPosition(2);
+        switch (state) {
+            case (0):
+                shooter.primeHighSpeed();
+                if(shooter.getCurrentCommand().toString().equals("ShooterOff")){
+                    if (shotNumber<3){
+                        new Shoot(20,180).start();
+                        shotNumber++;
+                    }
+                    else{
+                        shotNumber=0;
+                        shooter.disable();
+                        state++;
+                    }
+                }
+                break;
+            case (1):
+                if (driveTrain.driveStraight(pref.getDouble("AutoDist_0", 0.0))) {
+                    state++;
+                }
+                break;
+            case (2):
+                state++;
+                break;
+            case (3):
+                if (driveTrain.driveStraight(pref.getDouble("AutoDist_1", 0.0))) {
+                    state++;
+                }
+                break;
+            case (4):
+                state++;
+                break;
+            case (5):
+                if (driveTrain.driveStraight(pref.getDouble("AutoDist_2", 0.0))) {
+                    state++;
+                }
+                break;
+            case (6):
+                shooter.primeHighSpeed();
+                if(shooter.getCurrentCommand().toString().equals("ShooterOff")){
+                    if (shotNumber<4){
+                        new Shoot(20,180).start();
+                        shotNumber++;
+                    }
+                    else{
+                        shotNumber=0;
+                        shooter.disable();
+                        state++;
+                    }
+                }
+                break;
+            case (7):
+                if (driveTrain.driveStraight(pref.getDouble("AutoDist_3", 0.0))) {
+                    state++;
+                }
+                break;
+            case (8):
+                state++;
+                break;
+            case (9):
+                if (driveTrain.driveStraight(pref.getDouble("AutoDist_4", 0.0))) {
+                    state++;
+                }
+                break;
+            case (10):
+                shooter.primeHighSpeed();
+                if(shooter.getCurrentCommand().toString().equals("ShooterOff")){
+                    if (shotNumber<2){
+                        new Shoot(20,180).start();
+                        shotNumber++;
+                    }
+                    else{
+                        shotNumber=0;
+                        shooter.disable();
+                        state++;
+                    }
+                }
+                new Shoot(10,180).start();
+                break;
+            default:
+                driveTrain.arcadeDrive(0, 0);
+        }
+    }
+
+    protected boolean isFinished() {
+        return state>10;
+    }
+
+    protected void end() {
+        driveTrain.arcadeDrive(0, 0);
+        intake.disable();
+    }
+
+    protected void interrupted() {
+        end();
+    }
 }//end Autonomous
--- Mk3/Mk3/src/org/usfirst/frc100/Mk3/subsystems/DriveTrain.java
+++ Mk3/Mk3/src/org/usfirst/frc100/Mk3/subsystems/DriveTrain.java
@@ -11,15 +11,14 @@ import org.usfirst.frc100.Mk3.commands.Drive;
  * @author Team100
  */
 public class DriveTrain extends Subsystem implements SubsystemControl {
+    
     //Robot parts
     private final Talon rightMotor = RobotMap.driveRightMotor;
     private final Talon leftMotor = RobotMap.driveLeftMotor;
     private final Encoder rightEncoder = RobotMap.driveRightEncoder;
     private final Encoder leftEncoder = RobotMap.driveLeftEncoder;
     private final Gyro gyro = RobotMap.driveGyro;
-    private final RobotDrive robotDrive=RobotMap.driveRobotDrive;
-    //Constants
-    private final double kUltraDistRatio = 0.009794921875;
+    private final RobotDrive robotDrive = RobotMap.driveRobotDrive;
     //Preferences defaults
     private final boolean kDefaultReverseDirection = false;//need to change this for comp bot b/c now driving "backward"
     private final double kDefaultShootLimitVoltage = 1.12;//was 1.2 for comp bot
@@ -27,9 +26,15 @@ public class DriveTrain extends Subsystem implements SubsystemControl {
     private final double kDefaultQuickTurnDeadband = 0.19;
     //Other
     private boolean hasBeenAchieved = false;
-    
+    private double encoderVal;
+    private double encoderErr;
+    private double distOut;
+    private double gyroErr;
+    private double L_encoderVal;
+    private double R_encoderVal;
+
     //starts encoders
-    public DriveTrain(){
+    public DriveTrain() {
         leftEncoder.start();
         rightEncoder.start();
         gyro.reset();
@@ -46,31 +51,67 @@ public class DriveTrain extends Subsystem implements SubsystemControl {
         if (!p.containsKey("DriveTrainReverseDirection")) {
             p.putBoolean("DriveTrainReverseDirection", kDefaultReverseDirection);
         }
+        if (!p.containsKey("LeftEncoderRatio")) {
+            p.putDouble("LeftEncoderRatio", 233.0);
+        }
+        if (!p.containsKey("RightEncoderRatio")) {
+            p.putDouble("RightEncoderRatio", 158.0);
+        }
+        if (!p.containsKey("Encoder_kP")) {
+            p.putDouble("Encoder_kP", 0.5);
+        }
+        if (!p.containsKey("OutputMin")) {
+            p.putDouble("OutputMin", .2);
+        }
+        if (!p.containsKey("DistBuffer")) {
+            p.putDouble("DistBuffer", 0.06);
+        }
+        if (!p.containsKey("Gyro_kP")) {
+            p.putDouble("Gyro_kP", 1 / 18);
+        }
+        if (!p.containsKey("AngleBuffer")) {
+            p.putDouble("AngleBuffer", 4.5);
+        }
+        if (!p.containsKey("AutoDist_0")) {
+            p.putDouble("AutoDist_0", 0.0);
+        }
+        if (!p.containsKey("AutDist_1")) {
+            p.putDouble("AutoDist_1", 0.0);
+        }
+        if (!p.containsKey("AutoDist_2")) {
+            p.putDouble("AutoDist_2", 0.0);
+        }
+        if (!p.containsKey("AutoDist_3")) {
+            p.putDouble("AutoDist_3", 0.0);
+        }
+        if (!p.containsKey("AutoDist_4")) {
+            p.putDouble("AutoDist_4", 0.0);
+        }
     }//end constructor
-    
+
     //creates a new Drive
     public void initDefaultCommand() {
         setDefaultCommand(new Drive());
     }//end initDefaultCommand
-    
-    public void tankDrive(double leftSpeed, double rightSpeed){
+
+    public void tankDrive(double leftSpeed, double rightSpeed) {
         Preferences p = Preferences.getInstance();
         final boolean kReverseDirection = p.getBoolean("DriveTrainReverseDirection", false);
         robotDrive.setInvertedMotor(RobotDrive.MotorType.kRearLeft, kReverseDirection);
         robotDrive.setInvertedMotor(RobotDrive.MotorType.kRearRight, kReverseDirection);
         robotDrive.tankDrive(leftSpeed, rightSpeed);
     }//end tankDrive
-    
+
     //basic arcadeDrive: y=forward/backward speed, x=left/right speed
-    public void arcadeDrive(double y, double x){
+    public void arcadeDrive(double y, double x) {
         Preferences p = Preferences.getInstance();
         final boolean kReverseDirection = p.getBoolean("DriveTrainReverseDirection", false);
         robotDrive.setInvertedMotor(RobotDrive.MotorType.kRearLeft, kReverseDirection);
         robotDrive.setInvertedMotor(RobotDrive.MotorType.kRearRight, kReverseDirection);
-        robotDrive.arcadeDrive(y, x+0.05);
+        robotDrive.arcadeDrive(y, x + 0.05);
         SmartDashboard.putNumber("DriveTrainGyro", -gyro.getAngle());//upside down
     }// end arcadeDrive
-    
+
     public void resetGyro() {
         gyro.reset();
     }//end resetGyro
@@ -78,11 +119,11 @@ public class DriveTrain extends Subsystem implements SubsystemControl {
     public double getGyro() {
         return gyro.getAngle();
     }//end getGyro
-    
-    public void resetRangefinder(){
+
+    public void resetRangefinder() {
         hasBeenAchieved = false;
     }//end resetRangefinder
-    
+
     public boolean quickTurn(double setpoint) {
         // Proportional quickturn algorithm
         Preferences p = Preferences.getInstance();
@@ -92,33 +133,50 @@ public class DriveTrain extends Subsystem implements SubsystemControl {
         double error = setpoint - angle;
         double twist = error * kP;
         double magnitude = Math.abs(twist);
-        if(magnitude < kDB) {
-            twist = kDB * (twist>0.0 ? 1.0:-1.0);
+        if (magnitude < kDB) {
+            twist = kDB * (twist > 0.0 ? 1.0 : -1.0);
         } else if (magnitude > 1.0) {
-            twist = (twist>0.0 ? 1.0:-1.0);
+            twist = (twist > 0.0 ? 1.0 : -1.0);
         }
-        
+
         SmartDashboard.putNumber("DriveTrainGyro", angle);
-        
+
         leftMotor.set(-twist);
         rightMotor.set(twist);
- 
+
         // Done once we pass the setpoint
         return Math.abs(angle) >= Math.abs(setpoint);
     }//end quickTurn
-    
-    public void disable(){
+
+    public boolean driveStraight(double dist) {
+        Preferences p = Preferences.getInstance();
+        L_encoderVal = leftEncoder.get() / p.getDouble("LeftEncoderRatio", 0.0); // converts encoder value to feet
+        R_encoderVal = rightEncoder.get() / p.getDouble("RightEncoderRatio", 0.0); // converts encoder value to feet
+        encoderVal = (L_encoderVal + R_encoderVal) / 2; // averages out encoder values
+        encoderErr = dist - encoderVal;
+        distOut = encoderErr * p.getDouble("Encoder_kP", 0.0) + (Math.abs(dist) / dist) * p.getDouble("OutputMin", 0.0); // Encoder kP = 0.5; abs(x)/x returns sign of x; 0.2 is the min. magnitude
+        gyroErr = gyro.getAngle(); // Setpoint is always 0
+        if (Math.abs(encoderErr) < p.getDouble("DistBuffer", 0.0) && Math.abs(gyroErr) < p.getDouble("AngleBuffer", 0.0)) {
+            leftEncoder.reset();
+            rightEncoder.reset();
+            return true; // returns true when robot gets to its goal
+        } else {
+            robotDrive.arcadeDrive(distOut, gyroErr * p.getDouble("gyro_kP", 0.0)); // Gyro kP = 1/18.0; Arcade Drive uses reversed rotate values (neg. goes Left / pos. goes Right)
+            return false; // returns false if robot still hasn't reached its goal yet
+        }
+    }//end driveStraight
+
+    public void disable() {
         leftMotor.set(0.0);
         rightMotor.set(0.0);
         rightEncoder.reset();
         leftEncoder.reset();
     }//end disable
-    
-    public void enable(){
+
+    public void enable() {
         rightEncoder.reset();
         leftEncoder.reset();
         rightEncoder.start();
         leftEncoder.start();
     }//end enable
-    
 }//end DriveTrain
