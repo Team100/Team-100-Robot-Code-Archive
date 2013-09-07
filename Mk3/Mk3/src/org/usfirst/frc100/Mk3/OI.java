04a0162948799c24e2ca3ede218ec9114f9ab52e
Paul <Paul@localhost>
2013-09-07 11:31:11 -0700

    Moved some files around in the repository.

---
+++ Mk3/Mk3/src/org/usfirst/frc100/Mk3/OI.java
@@ -0,0 +1,65 @@
+package org.usfirst.frc100.Mk3;
+
+import edu.wpi.first.wpilibj.*;
+import edu.wpi.first.wpilibj.buttons.*;
+import org.usfirst.frc100.Mk3.commands.*;
+
+/**
+ * This class is the glue that binds the controls on the physical operator
+ * interface to the commands and command groups that allow control of the robot.
+ */
+public class OI {
+    
+    //Joystick declarations
+    public static final Joystick driverLeft = new Joystick(1);
+    public static final Joystick driverRight = new Joystick(2);
+    public static final Joystick manipulator = new Joystick(3);
+    
+    //Button declarations
+    //DriverLeft button declarations
+    //public static final JoystickButton straightShootButton = new JoystickButton(driverLeft, 1);
+    public static final JoystickButton hangButton = new JoystickButton(driverLeft, 1);
+    
+    //DriverRight button declarations
+    public static final JoystickButton shiftGearsButton = new JoystickButton(driverRight, 1);
+    //public static final JoystickButton quickTurnButton = new JoystickButton(driverRight, 2);
+    
+    //Manipulator button declarations
+    public static final JoystickButton runIntakeButton = new JoystickButton(manipulator, 1);
+    public static final JoystickButton tiltIntakeUpButton = new JoystickButton(manipulator, 2);
+    public static final JoystickButton tiltIntakeDownButton = new JoystickButton(manipulator, 3);
+    public static final JoystickButton tiltIntakeTestButton = new JoystickButton(manipulator, 4);
+    public static final JoystickButton tiltUpButton = new JoystickButton(manipulator, 5);
+    public static final JoystickButton shootButton = new JoystickButton(manipulator, 6);
+    public static final JoystickButton tiltDownButton = new JoystickButton(manipulator, 7);
+    public static final JoystickButton primeHighSpeedButton = new JoystickButton(manipulator, 8);
+    public static final JoystickButton unjamButton = new JoystickButton(manipulator, 9);
+    public static final JoystickButton primeLowSpeedButton = new JoystickButton(manipulator, 10);
+    
+    public OI() {  
+        //Assigning commands to buttons
+
+        //DriverLeft commands
+        //straightShootButton.whileHeld(new AlignToShoot());
+        hangButton.whenPressed(new ToggleHanger());
+        
+        //DriverRight commands
+        shiftGearsButton.whileHeld(new ShiftGears());
+        shiftGearsButton.whenReleased(new ShiftGearsBack());
+        //quickTurnButton.whileHeld(new QuickTurn());
+        
+        //Manipulator commands
+        //runIntakeButton.whileHeld(new RunIntake());
+        runIntakeButton.whenPressed(new DriveStraight());
+        tiltIntakeUpButton.whenPressed(new TiltIntake(1));
+        tiltIntakeDownButton.whenPressed(new TiltIntake(2));
+        tiltIntakeTestButton.whenPressed(new TiltIntake(3));
+        tiltUpButton.whenPressed(new TiltUp());
+        shootButton.whileHeld(new Shoot(0,180));
+        tiltDownButton.whenPressed(new TiltDown());
+        primeHighSpeedButton.whileHeld(new PrimeHighSpeed());
+        unjamButton.whileHeld(new UnjamFrisbees());
+        primeLowSpeedButton.whileHeld(new PrimeLowSpeed());
+    }//end constructor
+    
+}//end OI
---
+++ Mk3/Mk3/src/org/usfirst/frc100/Mk3/commands/QuickTurn.java
@@ -0,0 +1,69 @@
+
+//QuickTurn quickly turns OrangaHang a variable amount of degrees. 
+//we will probably set it to 90 degrees.
+
+package org.usfirst.frc100.Mk3.commands;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import org.usfirst.frc100.Mk3.OI;
+
+/**
+ *
+ * @author Team100
+ */
+public class QuickTurn extends CommandBase
+{
+    private boolean isFinished = false;
+    private double setpoint;
+    
+    public QuickTurn()
+    {
+        // Use requires() here to declare subsystem dependencies
+        requires(driveTrain);
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize()
+    {
+        driveTrain.resetGyro();
+        // Joystick position determines direction of the turn
+        if(Math.abs(OI.driverRight.getX()) < 0.1)
+        {
+            setpoint = 0.0;
+            isFinished = true;
+        }
+        else
+        {
+            setpoint = 90.0 * (OI.driverRight.getX()>0.0 ? 1.0:-1.0);
+            isFinished = false;
+        }
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute()
+    {
+        if (isFinished) {
+            return;
+        }
+        isFinished = driveTrain.quickTurn(setpoint);
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished()
+    {
+        return isFinished;
+    }
+
+    // Called once after isFinished returns true
+    protected void end()
+    {
+        driveTrain.disable();
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() 
+    {
+        end();
+    }
+}
