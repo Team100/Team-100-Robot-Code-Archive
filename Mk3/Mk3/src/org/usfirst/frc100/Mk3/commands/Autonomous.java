2ed171c5aa4c60718fbf2ce8814d8d4b0ea0afa0
Team100 <Team100@Cien>
2013-11-10 15:41:27 -0800

    Madtown changes. Now uses regular auto mode.

--- Mk3/Mk3/src/org/usfirst/frc100/Mk3/commands/Autonomous.java
+++ Mk3/Mk3/src/org/usfirst/frc100/Mk3/commands/Autonomous.java
@@ -14,13 +14,19 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 public class Autonomous extends CommandGroup {
     
     Preferences p = Preferences.getInstance();
+    boolean fiveDisc=true;
+    double autoTime0=3, autoTime1=1.5, autoTime2=3.5, autoTime3=3;
+    double kInitialDelay = 2.5;//may use a different value
+    double kTimeout = 5;//may use a different value
+    
+    //Time taken for 5-disc = kTimeout+autoTime0+autoTime3+kTimeOut-(kInitialDelay or autoTime3, whichever is less)
+    //Time taken for 7-disc = kTimeout+autoTime0+autoTime1+autoTime2+kTimeOut-(kInitialDelay or autoTime2, whichever is less)
+    //Currently, we have 7.5 seconds max for driving, although we should aim for 7
     
     public Autonomous() {
-        double kInitialDelay = 2.5;//may use a different value
-        double kTimeout = 5;//may use a different value
         
         this.addSequential(new LowerIntake());
-        this.addParallel(new DriveStraight(-100), .1);
+        this.addParallel(new DriveStraight(-100), .1);//to lower intake
         
         this.addSequential(new TiltUp());
         this.addParallel(new PrimeHighSpeed(), kTimeout);
@@ -28,36 +34,32 @@ public class Autonomous extends CommandGroup {
         this.addSequential(new TiltDown());
         
         double setPoint = p.getDouble("AutoDist_0", 0.0);
-        this.addSequential(new DriveStraight(setPoint), 2);//may use a different value
+        this.addSequential(new DriveStraight(setPoint), autoTime0);//may use a different value
         
         this.addParallel(new LoadFrisbeeGroup());
-//        this.addSequential(new RaiseIntake());
-//        this.addSequential(new UseIntake());
-//        this.addParallel(new LowerIntake());
         
-        if(true){//5 disc
+        if(fiveDisc){//5 disc
+            this.addParallel(new PrimeHighSpeed(), 7+autoTime3);
+            
             setPoint+=p.getDouble("AutoDist_3", 0.0);
-            this.addSequential(new DriveStraight(setPoint), 3);//may use a different value
-            this.addParallel(new PrimeHighSpeed(), 7);
+            this.addSequential(new DriveStraight(setPoint), autoTime3);//may use a different value
+            
             this.addSequential(new TiltUp());
-            this.addSequential(new Shoot(kInitialDelay, 5));
+            this.addSequential(new Shoot(kInitialDelay-autoTime3, 7));
         }
         else{//7 disc
             setPoint+=p.getDouble("AutoDist_1", 0.0);
-            this.addSequential(new DriveStraight(setPoint), 1);//may use a different value
+            this.addSequential(new DriveStraight(setPoint), autoTime1);//may use a different value
             
-            this.addSequential(new RaiseIntake());
-            this.addSequential(new UseIntake());
-            this.addSequential(new LowerIntake());
+            this.addParallel(new LoadFrisbeeGroup());
+            
+            this.addParallel(new PrimeHighSpeed(), 7+autoTime2);
             
             setPoint+=p.getDouble("AutoDist_2", 0.0);
-            this.addSequential(new DriveStraight(setPoint), 1.5);//may use a different value
-            this.addParallel(new PrimeHighSpeed(), 7);
+            this.addSequential(new DriveStraight(setPoint), autoTime2);//may use a different value
+            
             this.addSequential(new TiltUp());
-            this.addSequential(new Shoot(kInitialDelay, 5));
+            this.addSequential(new Shoot(kInitialDelay-autoTime2, 7));
         }
-
-        
-        //raise intake command
     }
 }
