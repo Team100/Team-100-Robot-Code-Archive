2a38132bd0edd6b45a0f256966cca44e998de8d8
Paul <Paul@localhost>
2014-01-12 16:27:38 -0800

    Intake code completed.

--- Robot2014/Robot2014/src/org/usfirst/frc100/Robot2014/commands/RunIntakeOut.java
+++ Robot2014/Robot2014/src/org/usfirst/frc100/Robot2014/commands/RunIntakeOut.java
@@ -1,3 +1,4 @@
+//ready
 package org.usfirst.frc100.Robot2014.commands;
 
 import edu.wpi.first.wpilibj.command.Command;
@@ -18,6 +19,7 @@ public class RunIntakeOut extends Command {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
+        Robot.intake.runOut();
     }
 
     // Make this return true when this Command no longer needs to run execute()
@@ -27,10 +29,12 @@ public class RunIntakeOut extends Command {
 
     // Called once after isFinished returns true
     protected void end() {
+        Robot.intake.stop();
     }
 
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     protected void interrupted() {
+        end();
     }
 }
