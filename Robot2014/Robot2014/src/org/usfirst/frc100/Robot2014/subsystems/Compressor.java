ea0f53d475c504ff0225cc4970ad54e23c1bfd28
daniel <daniel@localhost>
2014-01-18 15:31:23 -0800

    package org.usfirst.frc100.Robot2014.subsystems;
    import org.usfirst.frc100.Robot2014.RobotMap;
    import org.usfirst.frc100.Robot2014.commands.*;
    import edu.wpi.first.wpilibj.*;
    import edu.wpi.first.wpilibj.command.Subsystem;
    
    /**
     * Creates and starts the compressor.
     */
    public class Compressor extends Subsystem {
    
        edu.wpi.first.wpilibj.Compressor compressor = RobotMap.compressor;
    
        public void startCompressor() {
            compressor.start();
        }
    
        public void stopCompressor() {
            compressor.stop();
        }
    
        // Put methods for controlling this subsystem
        // here. Call these from Commands.
        public void initDefaultCommand() {
            // BEGIN AUTOGENERATED CODE, SOURCE=ROBOTBUILDER ID=DEFAULT_COMMAND
            // END AUTOGENERATED CODE, SOURCE=ROBOTBUILDER ID=DEFAULT_COMMAND
            // Set the default command for a subsystem here.
            //setDefaultCommand(new MySpecialCommand());
        }
    }

--- Robot2014/Robot2014/src/org/usfirst/frc100/Robot2014/subsystems/Compressor.java
+++ Robot2014/Robot2014/src/org/usfirst/frc100/Robot2014/subsystems/Compressor.java
@@ -12,6 +12,14 @@ public class Compressor extends Subsystem {
 
     edu.wpi.first.wpilibj.Compressor compressor = RobotMap.compressor;
 
+    public void startCompressor() {
+        compressor.start();
+    }
+
+    public void stopCompressor() {
+        compressor.stop();
+    }
+
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
     public void initDefaultCommand() {
