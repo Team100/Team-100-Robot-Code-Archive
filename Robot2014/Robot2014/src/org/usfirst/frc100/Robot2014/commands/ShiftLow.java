54ef1d3e70a6b922ce3cee18ca6cffa5d50f5ed1
daniel <daniel@localhost>
2014-01-18 15:09:30 -0800

    Code written for shifting to low gear.

--- Robot2014/Robot2014/src/org/usfirst/frc100/Robot2014/commands/ShiftLow.java
+++ Robot2014/Robot2014/src/org/usfirst/frc100/Robot2014/commands/ShiftLow.java
@@ -8,12 +8,15 @@ import org.usfirst.frc100.Robot2014.Robot;
  * while held.
  */
 public class ShiftLow extends Command {
+   
 
     public ShiftLow() {
+        
     }
 
     // Called just before this Command runs the first time
     protected void initialize() {
+        Robot.driveTrain.shiftLow();
     }
 
     // Called repeatedly when this Command is scheduled to run
@@ -27,10 +30,12 @@ public class ShiftLow extends Command {
 
     // Called once after isFinished returns true
     protected void end() {
+        Robot.driveTrain.shiftHigh();
     }
 
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     protected void interrupted() {
+        end();
     }
 }
