2acddf453de6bf51badbfa5f6ee7998c550722b8
Matt <Matt@localhost>
2014-01-25 11:05:59 -0800

    The ultrasonic alignToShoot works! Finally! Geez this took a while...

--- Robot2014/Robot2014/src/org/usfirst/frc100/Robot2014/commands/AlignToShoot.java
+++ Robot2014/Robot2014/src/org/usfirst/frc100/Robot2014/commands/AlignToShoot.java
@@ -9,8 +9,14 @@ import org.usfirst.frc100.Robot2014.Robot;
  * Command is while held.
  */
 public class AlignToShoot extends Command {
-    
+
     boolean complete = false;
+    double findub;
+    double findub2;
+    double storedDist;
+//    boolean take2trigger;
+    boolean take2complete;
+    int counter;
 
     public AlignToShoot() {
         requires(Robot.driveTrain);
@@ -18,38 +24,74 @@ public class AlignToShoot extends Command {
 
     // Called just before this Command runs the first time
     protected void initialize() {
-         Robot.driveTrain.resetRangefinder();
+        Robot.driveTrain.resetRangefinder();
+        complete = false;
+//        take2trigger = false;
+        counter = 0;
+        storedDist = 0;
+        //take2 = true;
+
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
 
-       double tempInches = Robot.driveTrain.getRangeInches();
-       if(tempInches<0)
-       {
-           Robot.driveTrain.stop();
-           return;
-       }
+        double tempInches = Robot.driveTrain.getRangeInches();
+//            System.out.println("tempInches " + tempInches);
+//        if (tempInches < 0) {
+//            Robot.driveTrain.stop();
+//            return;
+//        }
 
-       if (tempInches > Preferences.ultraStopDistance) {
+        if ((tempInches > Preferences.ultraInitialStopDistance||tempInches<0) &&!complete) {
             if (Preferences.tankDriveMode) {
                 Robot.driveTrain.tankDrive(Robot.oi.getDriverLeft().getY(), Robot.oi.getDriverRight().getY());
             } else {
                 Robot.driveTrain.arcadeDrive(Robot.oi.getDriverLeft().getY(), Robot.oi.getDriverRight().getX());
             }
-        } else if(complete == false) {
-            if(Robot.driveTrain.autoTurnToAngle(0)){
-                complete = true;
-                Robot.driveTrain.setDirection();
-                Robot.driveTrain.resetEncoders();
+        } else if (complete == false) {
+            if (Robot.driveTrain.autoTurnToAngle(0)) {
+                //Robot.driveTrain.stop();
+                
+                if(Robot.driveTrain.getDistance() == storedDist)
+                {
+                    counter++;
+                }
+                
+                storedDist = Robot.driveTrain.getDistance();
+                
+                if(counter >= Preferences.inQuisitionCounter)
+                {
+                    complete = true;
+                    findub = tempInches - Preferences.ultraActualStopDistance;
+                    Robot.driveTrain.setDirection();
+                    Robot.driveTrain.resetEncoders();
+                    System.out.println("INITfindub " + findub);
+                System.out.println("INITtempInches " + tempInches);
+                System.out.println("INITpref " + Preferences.ultraActualStopDistance);
+                }
+                
+//                System.out.println("INITfindub " + findub);
+//                System.out.println("INITtempInches " + tempInches);
+//                System.out.println("INITpref " + Preferences.ultraActualStopDistance);
+                
+                
+
             }
-        }
+        } else {
+            
+//            System.out.println("findub " + findub);
+//            System.out.println("tempInches " + tempInches);
+            
+            Robot.driveTrain.autoDriveStraight(findub);
 
-        if (complete) {
-            //Robot.driveTrain.autoTurnToAngle(0);
-            Robot.driveTrain.autoDriveStraight(tempInches - Preferences.ultraStopDistance);
+            
+           /* if(Robot.driveTrain.autoDriveStraight(findub))
+            {
+                take2trigger = true;
+                Robot.driveTrain.stop(); 
+            }*/
         }
-
     }
 
     // Make this return true when this Command no longer needs to run execute()
