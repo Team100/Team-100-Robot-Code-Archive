643c6ee87024bff4b27a995116e39aa5b8e8e55e
Matt <Matt@localhost>
2014-02-02 13:00:14 -0800

    fixed a couple more new ultrasonic sensor things...

---
+++ OrangaHang/OrangaHang/OrangaHang/src/org/usfirst/frc100/OrangaHang/subsystems/PIDBundle/TimedThread.java
@@ -0,0 +1,41 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.usfirst.frc100.OrangaHang.subsystems.PIDBundle;
+
+import edu.wpi.first.wpilibj.Preferences;
+import edu.wpi.first.wpilibj.Timer;
+
+/**
+ *
+ * @author Isis
+ */
+public class TimedThread implements Runnable {
+
+    private Callable m_callable;
+    private String m_periodKey;
+    private final double kDefaultPeriod=.05;
+
+    public TimedThread(Callable callable, String periodKey) {
+        this.m_callable = callable;
+        this.m_periodKey = periodKey;
+        Preferences p =Preferences.getInstance();
+        if (!p.containsKey(m_periodKey)){
+            p.putDouble(m_periodKey, kDefaultPeriod);
+        }
+    }
+
+    public void run() {
+        while (true) {
+            m_callable.call();
+            double period= Preferences.getInstance().getDouble(m_periodKey, kDefaultPeriod);
+            Timer.delay(period);
+        }
+    }
+
+    public void start() {
+        new Thread(this).start();
+    }
+    
+}//end TimedThread
