3af039defc7e191fd95daa0aeeab2b410f20838c
Koosha Jahani <Koosha Jahani@localhost>
2014-02-11 17:37:40 -0800

    Our next step is to hopefully multithread the arduino

---
+++ Ballrus/Ballrus/src/org/usfirst/frc100/Ballrus/commands/AutoDriveStraight.java
@@ -0,0 +1,63 @@
+//ready
+package org.usfirst.frc100.Ballrus.commands;
+
+import edu.wpi.first.wpilibj.command.Command;
+import org.usfirst.frc100.Ballrus.Preferences;
+import org.usfirst.frc100.Ballrus.Ballrus;
+
+/**
+ * Drives the robot a given distance in a straight line, then stops the robot
+ * and terminates. Parameter is the distance in inches to drive.
+ */
+public class AutoDriveStraight extends Command {
+
+    private int inPositionCounter;
+    private double distance;
+    private double lastDist;
+    
+    public AutoDriveStraight(double distance) {
+        requires(Ballrus.driveTrain);
+        this.distance=distance;
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+        Ballrus.driveTrain.setDirection();
+        Ballrus.driveTrain.resetEncoders();
+        inPositionCounter = 0;
+        lastDist = 0.0;
+    }
+    
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+        double realDist = Ballrus.driveTrain.getEncoderInches();
+        if(Ballrus.driveTrain.autoDriveStraight(distance)){
+            if (Math.abs(lastDist - realDist) < 1) {
+                inPositionCounter++;
+            } else {
+                lastDist = realDist;
+                inPositionCounter = 0;
+            }
+        }
+        else{
+            inPositionCounter = 0;
+        }
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return inPositionCounter>Preferences.autoDriveDelay;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+        Ballrus.driveTrain.stop();
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+        end();
+    }
+}
---
+++ OrangaHang/Mk3/Mk3/src/org/usfirst/frc100/Mk3/commands/Drive.java
@@ -0,0 +1,57 @@
+package org.usfirst.frc100.Mk3.commands;
+
+import edu.wpi.first.wpilibj.Preferences;
+import edu.wpi.first.wpilibj.Timer;
+import org.usfirst.frc100.Mk3.OI;
+
+public class Drive extends CommandBase {
+
+    boolean isTurning = false; //Uses variable so that it doesn't interfere with shift button
+    Preferences p = Preferences.getInstance();
+    Timer t = new Timer();
+
+    public Drive() {
+        // Use requires() here to declare subsystem dependencies
+        requires(driveTrain);
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+        //Because our robot only has 4 wheels we have to shift down whenever we turn
+        
+//    For ArcadeDrive:
+//        if (Math.abs(OI.driverRight.getX()) > p.getDouble("ShiftWhenTurningThreshold", 0.5) && !isTurning) {
+//            shifter.shiftLowGear(); //Shifts down when the driver starts to turn
+//            t.reset();
+//            t.start();
+//            isTurning = true;
+//        }
+//        if (Math.abs(OI.driverRight.getX()) <= p.getDouble("ShiftWhenTurningThreshold", 0.5) && isTurning && t.get() > p.getDouble("ShiftWhenTurningDelay", 1.5)) {
+//            shifter.shiftHighGear(); //Shifts back up when the driver stops turning
+//            isTurning = false;
+//        }
+          driveTrain.arcadeDrive(OI.driverLeft.getY(), OI.driverRight.getX());
+//        driveTrain.tankDrive(OI.driverLeft.getY(), OI.driverRight.getY()); //Double joystick arcade drive
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return false; //default command
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+        // We don't stop the drivetrain motors here in order to smoothly
+        // transition to AlignToShoot
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+        end();
+    }
+}
