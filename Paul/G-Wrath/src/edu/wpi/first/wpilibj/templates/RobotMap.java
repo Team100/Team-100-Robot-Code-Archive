ba0b2fafbc600f20fe5c10497c1b2455ab4d8d39
Ryan <Ryan@localhost>
2014-02-17 14:16:02 -0800

    Auto tuning

---
+++ Paul/G-Wrath/src/edu/wpi/first/wpilibj/templates/RobotMap.java
@@ -0,0 +1,45 @@
+//Assigns variables to the different channels
+package edu.wpi.first.wpilibj.templates;
+
+public class RobotMap {
+
+    public static final int leftjoystick = 1;
+    public static final int rightjoystick = 2;
+    public static final int leftjoystickbutton1=1;
+    public static final int leftjoystickbutton2=2;
+    public static final int rightjoystickbutton1=1;
+    public static final int rightjoystickbutton2=2;
+    public static final int dio_rightencoderA=1;
+    public static final int dio_rightencoderB=2;
+    public static final int dio_leftencoderB=3;
+    public static final int dio_leftencoderA=4;
+    public static final int dio_linefollowingleft=5;
+    public static final int dio_linefollowingcenter=6;
+    public static final int dio_linefollowingright=7;
+    public static final int dio_linefollowingback=8;
+    public static final int dio_compressor=11;
+    public static final int ai_gyro=1;
+    public static final int ai_irdepthsensor1=2;
+    public static final int ai_irdepthsensor2=3;
+    public static final int ai_irdepthsensor3=4;
+    public static final int ai_lightsensor=6;
+    public static final int ai_armpotentiometer=7;
+    public static final int pwm_camerapanservo=1;
+    public static final int pwm_cameratiltservo=2;
+    public static final int pwm_lowjawmotor=3;
+    public static final int pwm_highjawmotor=4;
+    public static final int pwm_shouldermotor=5;
+    public static final int pwm_leftdriveA=6;
+    public static final int pwm_leftdriveB=7;
+    public static final int pwm_rightdriveA=8;
+    public static final int pwm_rightdriveB=9;
+    public static final int relay_compressor=1;
+    public static final int relay_leds=2;
+    public static final int sol_rightshifter=2;
+    public static final int sol_minibot=3;
+    public static final int sol_leds=4;
+    public static final int sol_leftshifter=5;
+    public static final int sol_wrist1=6;
+    public static final int sol_wrist2=7;
+
+}
