255d2d2afb826d0a215c49ff0091b590e24c51e5
Koosha Jahani <Koosha Jahani@localhost>
2014-09-06 14:43:51 -0700

    Heavily improved code

--- Paul/PID_PreferencesTest/src/edu/wpi/first/wpilibj/templates/Preferences.java
+++ Paul/PID_PreferencesTest/src/edu/wpi/first/wpilibj/templates/Preferences.java
@@ -7,54 +7,62 @@ import java.io.IOException;
 import javax.microedition.io.Connector;
 
 /**
- * Stores all changeable or arbitrary values in a file on the cRIO. If code 
+ * Stores all changeable or arbitrary values in a file on the cRIO. If code
  * contains any number besides 0 or 1, chances are it should be a preference.
  */
 public class Preferences {
-    
+
     private static String file = ""; // Duplicate of the entire preference file
-    
+
 ////Preference Input/Output/////////////////////////////////////////////////////
-    
     // <editor-fold>
     // Creates or sets a preference, but does not modify cRIO file
-    public static void setPreference(String name, Object value){
+    public static void setPreference(String name, Object value) {
         if ("".equals(file)) {
             System.out.println("ERROR: File is empty");
             return;
         }
-        int startIndex = file.indexOf("\n" + name + " ", 0) + 1;
+        int startIndex = file.indexOf("\n" + name + " ") + 1;
         int endIndex = file.indexOf("\n", startIndex) - 1;
         if (startIndex <= 0 || endIndex <= 0) {
             file = file + "\n" + name + " " + value;
             System.out.println("Preference " + name + " added");
-        } else{
+        } else {
             file = file.substring(0, startIndex) + name + " " + value + file.substring(endIndex, file.length());
         }
     }
-    
+
     // Casts double to an object for regular setPreference method
-    public static void setPreference(String name, double value){
+    public static void setPreference(String name, double value) {
         setPreference(name, Double.valueOf(value));
     }
     
-    // Returns value of a double preference
-    public static double getDouble(String name){
-        return Double.parseDouble(getString(name));
+    // Casts boolean to an object for regular setPreference method
+    public static void setPreference(String name, boolean value) {
+        setPreference(name, Boolean.valueOf(value));
     }
-    
-    // Returns value of a boolean preference
-    public static boolean getBoolean(String name){
-        return "true".equals(getString(name));
+
+    // Returns value of the double preference with the given name
+    public static double getDoubleValue(String name) {
+        return Double.parseDouble(getStringValue(name));
     }
-    
-    // Returns value of a String preference
-    public static String getString(String name){
+
+    // Returns value of the boolean preference with the given name
+    public static boolean getBooleanValue(String name) {
+        String value = getStringValue(name);
+        if (!(value.equalsIgnoreCase("true") || value.equalsIgnoreCase("false"))) {
+            System.out.println("ERROR: no boolean found: " + name + " " + value);
+        }
+        return "true".equals(value);
+    }
+
+    // Returns value of the String preference with the given name
+    public static String getStringValue(String name) {
         if ("".equals(file)) {
             System.out.println("ERROR: File is empty");
             return "0";
         }
-        int startIndex = file.indexOf("\n" + name + " ", 0) + 1;
+        int startIndex = file.indexOf("\n" + name + " ") + 1;
         int endIndex = file.indexOf("\n", startIndex) - 1;
         if (startIndex <= 0 || endIndex <= 0) {
             System.out.println("Preference " + name + " not found");
@@ -62,9 +70,9 @@ public class Preferences {
         }
         return file.substring(startIndex + name.length() + 1, endIndex);
     }
-    
+
     // Returns whether a preference is present
-    public static boolean containsKey(String name){
+    public static boolean containsKey(String name) {
         if ("".equals(file)) {
             System.out.println("ERROR: File is empty");
             return false;
@@ -74,12 +82,11 @@ public class Preferences {
         return !(startIndex <= 0 || endIndex <= 0);
     }
     // </editor-fold>
-    
+
 ////File Read/Write/////////////////////////////////////////////////////////////
-    
     // <editor-fold>
     // Pulls preferences from cRIO file, overwrites any existing preferences
-    public static void readFromFile(){
+    public static void readFromFile() {
         DataInputStream actualFile;
         FileConnection fc;
         file = "";
@@ -87,7 +94,7 @@ public class Preferences {
             fc = (FileConnection) Connector.open("file:///Preferences.txt", Connector.READ);
             actualFile = fc.openDataInputStream();
             //RobotMap.stopAllMotors(); // just a precaution...this takes a while
-            for (int i = 0; i < 10000; i++) {
+            for (int i = 0; i < 16384; i++) {
                 int nextChar = actualFile.read();
                 if (nextChar == -1) {
                     break;
@@ -98,9 +105,9 @@ public class Preferences {
             System.out.println("ERROR: File Reading Failed");
         }
     }
-    
+
     // Overwrites cRIO file with current preferences
-    public static void writeToFile(){
+    public static void writeToFile() {
         DataOutputStream actualFile;
         FileConnection fc;
         if ("".equals(file)) {
